6c07447dac530e148129b69df9114b821657c5c9
Nurbyek <49381152+Nurbyek@users.noreply.github.com>
2020-12-28 09:20:41 +0800

    Add review tab and change design

---
+++ bbs/myreview.php
@@ -0,0 +1,49 @@
+<?php
+include_once('./_common.php');
+
+include_once('./_head.php');
+
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+// add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
+add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
+
+if (!$is_member)
+    alert_close('회원만 조회하실 수 있습니다.');
+
+$g5['title'] = get_text($member['mb_nick']).'님의 스크랩';
+include_once(G5_PATH.'/head.sub.php');
+
+/* if($member['mb_id']){
+    global $g5;
+    $result = sql_query("select bo_table from {$g5['board_table']} where gr_id='attendance'");
+    
+    $list = array();
+    for($i=0; $row=sql_fetch_array($result); $i++)
+    {
+      $bo_table = $row['bo_table'];
+
+      $result1 = sql_query("select * from ".$g5['write_prefix'].$bo_table. " where mb_id = '{$member['mb_id']}'");
+      if($result1){
+          for($j=0;$row1 = sql_fetch_array($result1); $j++){
+            array[i][j]=$row1;
+          }
+        
+      }
+      
+    } 
+}  */
+
+
+$myreview_skin_path = get_skin_path('member', $config['cf_member_skin']);
+$myreview_skin_url  = get_skin_url('member', $config['cf_member_skin']);
+$skin_file = $myreview_skin_path.'/myreview.skin.php';
+
+if(is_file($skin_file)) {
+    include($skin_file);
+} else {
+    echo '<p>'.str_replace(G5_PATH.'/', '', $skin_file).'이 존재하지 않습니다.</p>';
+}
+
+    include_once('./_tail.php');
+?>
---
+++ skin/member/NB-Basic/myreview.skin.php
@@ -0,0 +1,160 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+// add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
+add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
+?>
+
+<nav id="user_cate" class="sly-tab font-weight-normal mb-2">
+		<div class="px-3 px-sm-0">
+			<div class="d-flex">
+				<div id="user_cate_list" class="sly-wrap flex-grow-1">
+					<ul id="user_cate_ul" class="sly-list d-flex border-left-0 text-nowrap">
+						<li>
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/userinfo.php" >
+                                <span>
+                                <i class="fa fa-user">
+                                회원정보
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/mypost.php">
+                                <span>
+                                <i class="fa fa-pencil-alt">
+                                내 글
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="py2 px-3" href= "#">
+                                <span>
+                                <i class="fa fa-book">
+                                파편조각 :
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/point.php">
+                                <span>
+                                <i class="fa fa-gem">
+                                파운드 : <b><?php echo number_format($member['mb_point']);?></b>
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/scrap.php">
+                                <span>
+                                <i class="fa fa-paperclip">
+                                스크랩
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/coupon_create.php">
+                                <span>
+                                <i class="fa fa-cubes">
+                                쿠폰지원
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="py2 px-3" href= "#">
+                                <span>
+                                <i class="fa fa-handshake">
+                                쿠폰관리
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <li class="active">
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/myreview.php">
+                                <span>
+                                <i class="fa fa-pencil-alt">
+                                후기보기
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <hr/>
+				</div>
+			</div>
+		</div>
+    </nav>
+<!-- 스크랩 목록 시작 { -->
+
+<section id="bo_list" class="mb-4"> 
+	<div id="scrap_info" class="font-weight-normal px-3 pb-2 pt-4">
+		전체 <?php echo number_format($total_count) ?>건 / <?php echo $page ?>페이지
+	</div>
+
+	<div class="w-100 mb-0 bg-primary" style="height:4px;"></div>
+
+
+	<!-- 목록 헤드 -->
+	<div class="d-block d-md-none w-100 mb-10 bg-<?php echo $head_color ?>" style="height:4px;"></div>
+
+	<div class="na-table d-none d-md-table w-100 mb-0">
+		<div class="na-table-head border-primary d-md-table-row bg-light">
+			<div class="d-md-table-cell nw-4 px-md-1">그룹</div>
+			<div class="d-md-table-cell nw-6 px-md-1 pr-md-1">게시판</div>
+			<div class="d-md-table-cell nw-20 pl-2 px-md-1 pr-md-1">제목</div>
+			<div class="d-md-table-cell nw-6 pr-md-1">작성자</div>
+			<div class="d-md-table-cell nw-4 pr-md-1">일시</div>
+		</div>
+	</div>
+
+	<ul class="na-table d-md-table w-100">
+	<?php 
+	$result = sql_query("select bo_table from {$g5['board_table']} where gr_id='review'");
+    while ( $row=sql_fetch_array($result))
+    {
+        $bo_table = $row['bo_table'];
+        
+        $res = sql_fetch("select * from ".$g5['write_prefix'].$bo_table." where mb_id='{$member['mb_id']}'");
+        /* $bo = sql_fetch("select * from ".$g5['board_table']." where bo_table = '{$bo_table}'); */
+        if($res){  echo $row['bo_subject'];?>
+            
+                <li class="d-md-table-row px-3 py-2 p-md-0 text-md-center text-muted border-bottom">
+                    <div class="d-none d-md-table-cell nw-4 f-sm font-weight-normal py-md-2 px-md-1">
+                        <?php echo $res['ca_name'] ?> 
+                    </div>
+                    <div class="d-none d-md-table-cell nw-6 text-left f-sm font-weight-normal py-md-2 px-md-1">
+                        <a href="./write.php?w=u&bo_table=<?=$bo_table?>" style="color: #BFAF88;" >        
+                        <?php echo $row['bo_subject']; ?></a> 
+                    </div>
+                    <div class="float-right float-md-none d-md-table-cell nw-20 nw-md-auto text-left f-sm font-weight-normal pl-2 py-md-2 pr-md-1">
+                        <a href="./write.php?w=u&bo_table=<?=$bo_table?>&wr_id=<?=$res['wr_id']?>" style="color: #BFAF88;" >
+                            <?php echo $res['wr_subject']; ?>
+                        </a> 
+                    </div>
+                    <div class="float-left float-md-none d-md-table-cell nw-6 nw-md-auto f-sm font-weight-normal py-md-2 pr-md-1">
+                        <span class="sr-only">등록일</span>
+                        <?php echo $res['wr_name'] ?>
+                    </div>
+                    <div class="float-left float-md-none d-md-table-cell nw-4 nw-md-auto f-sm font-weight-normal py-md-2 pr-md-1">
+                            <?php echo $res['wr_datetime']; ?>
+                    </div>
+                    <div class="clearfix d-block d-md-none"></div>
+                </li>
+              <?php  }	
+    }	?> 
+		
+	</ul>
+	<?php if ($i==0) { ?>
+		<div class="f-de font-weight-normal px-3 py-5 text-muted text-center border-bottom">자료가 없습니다.</div>
+	<?php } ?>
+	<div class="font-weight-normal px-3 mt-4">
+		<ul class="pagination justify-content-center en mb-0">
+			<?php echo na_paging(G5_IS_MOBILE ? $config['cf_mobile_pages'] : $config['cf_write_pages'], $page, $total_page, "?$qstr&amp;page="); ?>
+		</ul>
+	</div>
+</section>  
+
