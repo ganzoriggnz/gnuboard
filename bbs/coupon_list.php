123907183d7efeb31de204a079b7d4aeee788e50
Nurbyek <nuugii.k@gmail.com>
2021-01-10 15:07:35 +0800

    Create add, alert, send, delete codes
    Create add, alert, send, delete codes

--- bbs/coupon_list.php
+++ bbs/coupon_list.php
@@ -13,13 +13,13 @@ $bo_table = $_REQUEST['bo_table'];
 // 상수 선언
 $g5['table_prefix']        = "g5_"; // 테이블명 접두사
 $g5['coupon_table'] = $g5['table_prefix'] . "coupon";    // 쿠폰 테이블
-$g5['couponsent_table'] = $g5['table_prefix'] . "couponsent";    // 쿠폰 테이블
+$g5['coupon_sent_table'] = $g5['table_prefix'] . "coupon_sent";    // 쿠폰 테이블
 $g5['write_prefix']        = "g5_write_";
 $g5['bo_table'] = $g5['write_prefix'] . $bo_table;
 
-$co_created = date('Y-m-d H:i:s');
-$currentmonth = substr($co_created, 5, 2);
-$co_start = date_create($co_created);
+$co_created_datetime = G5_TIME_YMDHIS;
+$currentmonth = substr($co_created_datetime, 5, 2);
+$co_start = date_create($co_created_datetime);
 $s_begin_date = date_format($co_start, 'Y-m-01 00:00:00');
 
 if($currentmonth == '01')
@@ -47,13 +47,13 @@ $s_end_date = date_format($co_start, 'Y-m-30 23:59:59');
 else if($currentmonth == '12')
 $s_end_date = date_format($co_start, 'Y-m-31 23:59:59');
 
-$result1 = "SELECT co_begin_date FROM $g5[coupon_table] WHERE co_begin_date='$s_begin_date' AND co_end_date='$s_end_date'";
+$result1 = "SELECT co_begin_datetime FROM $g5[coupon_table] WHERE co_begin_datetime='$s_begin_date' AND co_end_datetime='$s_end_date'";
 $sql = sql_fetch($result1);
-$date = $sql['co_begin_date'];
+$date = $sql['co_begin_datetime'];
 $year = substr($date, 0, 4);
 $month = substr($date, 5, 2);
 
-$result = "SELECT COUNT(a.co_no) as cnt FROM $g5[coupon_table] a INNER JOIN $g5[bo_table] b ON a.mb_id = b.mb_id WHERE a.co_begin_date='{$s_begin_date}' AND a.co_end_date='{$s_end_date}'"; 
+$result = "SELECT COUNT(a.co_no) as cnt FROM $g5[coupon_table] a INNER JOIN $g5[bo_table] b ON a.mb_id = b.mb_id WHERE a.co_begin_datetime='{$s_begin_date}' AND a.co_end_datetime='{$s_end_date}'"; 
 $row=sql_fetch($result);
 $total_count = $row['cnt'];
 
@@ -64,7 +64,7 @@ $from_record = ($page - 1) * $rows; // 시작 열을 구함
 
 $list = array();
 
-$sql = "SELECT * FROM $g5[coupon_table] a INNER JOIN $g5[bo_table] b ON a.mb_id = b.mb_id WHERE a.co_begin_date='{$s_begin_date}' AND a.co_end_date='{$s_end_date}' limit $from_record, $rows ";
+$sql = "SELECT * FROM $g5[coupon_table] a INNER JOIN $g5[bo_table] b ON a.mb_id = b.mb_id WHERE a.co_begin_datetime='{$s_begin_date}' AND a.co_end_datetime='{$s_end_date}' limit $from_record, $rows ";
 $result = sql_query($sql);
 for ($i=0; $row=sql_fetch_array($result); $i++) {
 
@@ -78,7 +78,8 @@ $coupon_list_skin_path = get_skin_path('coupon', 'NB-Basic');
 $coupon_list_skin_url  = get_skin_url('coupon', 'NB-Basic');
 $skin_file = $coupon_list_skin_path.'/coupon_list.skin.php';
 
-$couponsent_action_url = G5_BBS_URL.'/couponsent_form.php';
+$coupon_sent_action_url = G5_BBS_URL.'/coupon_sent_form.php';
+$coupon_delete_action_url = G5_BBS_URL.'/coupon_delete_form.php';
 
 if(is_file($skin_file)) {
     include($skin_file);
