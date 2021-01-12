927e474829ad733ecb2a8ba9eb06e3d7352b3f4f
ganzoriggnz <ganzoriggnz@gmail.com>
2021-01-12 12:38:27 +0800

    userinfo css changed

--- skin/member/NB-Basic/userinfo.skin.php
+++ skin/member/NB-Basic/userinfo.skin.php
@@ -97,17 +97,60 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 		</div>
     </nav>
     <section class="xm">
-        <dl class="dl-horizontal">
-            <dt><em>*</em> 기본정보</dt><dd style="font-weight:bold;"> <!-- <img src="//opssfriend.com/img/class/man/25.gif" alt="25">&nbsp; --><?php echo $row['mb_nick']." ".$row['mb_id']?></dd>
-            <dt>프로필 사진</dt>
-                    <dd>
-                        <a href="#" target="_blank" class="win_memo" title="사진등록">
+        <dl class="dl-horizontal text-left">
+        <!-- user  기본정보   mb_nick  -->
+        <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="reg_mb_nick">
+        <em>*</em> 기본정보       
+        </label>
+        <div class="col-sm-3 col-form-info">
+        <?php echo $row['mb_nick']." ".$row['mb_id']?>
+        </div>
+        </div>
+        <!-- 프로필 사진 user icon  -->
+        <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="reg_mb_nick">
+        프로필 사진     
+        </label>
+        <div class="col-sm-3 col-form-info">
+        <a href="#" target="_blank" class="win_memo" title="사진등록">
                             <div class="photo pull-left">
                                 <i class="fa fa-user"></i>				
                             </div>
-                        </a>
-                    </dd> 
-                    <?php if( $member['mb_level'] == 27)
+                        </a>       
+        </div>
+        </div>
+        <!-- user 출근부  -->
+        <?php if( $member['mb_level'] == 27)
+                    { ?>
+                   <div class="form-group row">
+                    <label class="col-sm-2 col-form-label" for="reg_mb_nick">출근부</label>
+                    <div class="col-sm-3 col-form-info">
+                    <?php
+                        $g5['connect_db'];
+                        $result = sql_query("select bo_table from {$g5['board_table']} where gr_id='attendance'");
+                        while ( $row=sql_fetch_array($result))
+                        {
+                            $bo_table = $row['bo_table'];
+                            
+                            $res = sql_fetch("select wr_id from ".$g5['write_prefix'].$bo_table." where mb_id='{$member['mb_id']}'");
+                            
+                            if($res){ ?>
+                                <?php if(defined('_INDEX_')) {?>    		
+                                <a href="./bbs/write.php?w=u&bo_table=<?=$bo_table?>&wr_id=<?=$res['wr_id']?>&page=" style="color: #BFAF88;" ><font color="blue"><b><i class="fa fa-pencil-square-o"></i> 업소정보 수정</b></font></a>
+                                <?php break;}
+                                else{?>    		
+                                <a href="./write.php?w=u&bo_table=<?=$bo_table?>&wr_id=<?=$res['wr_id']?>&page=" style="color: #BFAF88;" ><font color="blue"><b><i class="fa fa-pencil-square-o"></i> 업소정보 수정</b></font></a>
+                                <?php break;} ?>                              
+                            <?php }
+                            } ?>	
+                            </div>
+                        </div>
+                <?php } ?>
+
+
+           <!-- user icon  -->
+                    <!-- <?php if( $member['mb_level'] == 27)
                     { ?>
                     <dt>출근부</dt><dd><span style="font-size:15px; font-weight:900">     
                     <?php
@@ -129,11 +172,45 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
                             <?php }
                             } ?>	
                             </span></dd>
-                <?php } ?>
-                    
-                <dt>쿠폰</dt><dd><a href="<?php echo G5_BBS_URL ?>/coupon_create.php"><i class="fa fa-paperclip"></i> <span class="hidden-xs">쿠폰</span></a></dd>
-                <dt>지역-업종</dt><dd><?php echo $res['ca_name']." - ".$res['wr_5']; ?></dd>
-                <dt>제휴기간</dt><dd>2020-11-12 ~ 2021-02-12 <!-- - [ <a href="#" target="_blank"><font color="red"><b>3일 남음</b></font></a> ] --></dd> 
+                <?php } ?> -->
+                
+
+
+ <!-- user  쿠폰   coupon_create  -->
+ <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="reg_mb_nick">
+        쿠폰       
+        </label>
+        <div class="col-sm-3 col-form-info">
+        <a href="<?php echo G5_BBS_URL ?>/coupon_create.php"><i class="fa fa-paperclip"></i> <span class="hidden-xs">쿠폰</span></a>
+        </div>
+        </div>
+
+
+         <!-- user  지역-업종   mb_nick  -->
+         <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="reg_mb_nick">
+        지역-업종       
+        </label>
+        <div class="col-sm-3 col-form-info">
+        <?php echo $res['ca_name']." - ".$res['wr_5']; ?>
+        </div>
+        </div>
+
+
+ <!-- user  제휴기간   date  -->
+ <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="reg_mb_nick">
+        제휴기간      
+        </label>
+        <div class="col-sm-3 col-form-info">
+        2020-11-12 ~ 2021-02-12
+        </div>
+        </div>
+
+                <!-- <dt>쿠폰</dt><dd><a href="<?php echo G5_BBS_URL ?>/coupon_create.php"><i class="fa fa-paperclip"></i> <span class="hidden-xs">쿠폰</span></a></dd> -->
+                <!-- <dt>지역-업종</dt><dd><?php echo $res['ca_name']." - ".$res['wr_5']; ?></dd> 
+                <dt>제휴기간</dt><dd>2020-11-12 ~ 2021-02-12 - - [ <a href="#" target="_blank"><font color="red"><b>3일 남음</b></font></a> ] --></dd> 
                 <!-- <dt>제휴 연장 안내</dt>
                 <dd>
                     <p>제휴기간이 조만간 마감될 예정이니 운영자쪽지 혹은</p>
@@ -150,11 +227,42 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
                     <p>&nbsp;</p>
                     <p>&nbsp;</p>
                 </dd> --> 
-                <dt>연락처</dt><dd><?php if($row['mb_hp']) {echo $row['mb_hp'].'<br>'; }?>(프로필과 배너에 출력되니 항상 최신번호로 유지해주세요) &nbsp;&nbsp;&nbsp;<!-- <a onclick="window.open('//localhost/gnuboard/bbs/member_hp_change.php?mb_id=juho3141','전화번호 변경요청','width=300,height=300,scrollbars=no,padding=0, margin=0, top=300,left=800');" style="color:#000;background-color:#efefef; padding:5px; border:1px solid #696969; border-radius:5px; text-decoration:none">
+
+                <!-- user  연락처    -->
+ <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="reg_mb_nick">
+        연락처      
+        </label>
+        <div class="col-sm-3 col-form-info">
+        <?php if($row['mb_hp']) {echo $row['mb_hp'].'<br>'; }?>(프로필과 배너에 출력되니 항상 최신번호로 유지해주세요)
+        </div>
+        </div>
+
+        <!-- user  가입일   mb_datetime  -->
+        <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="reg_mb_nick">가입일</label>
+        <div class="col-sm-3 col-form-info">
+        <?php echo $row['mb_datetime'];?></div></div>
+
+        <!-- user  최근 로그인   mb_today_login  -->
+        <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="reg_mb_nick">최근 로그인</label>
+        <div class="col-sm-3 col-form-info">
+        <?php echo $row['mb_today_login'];?></div></div>
+
+        <!-- user 서명     -->
+        <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="reg_mb_nick">서명</label>
+        <div class="col-sm-3 col-form-info">
+        …</div></div>
+
+
+
+                <!-- <dt>연락처</dt><dd><?php if($row['mb_hp']) {echo $row['mb_hp'].'<br>'; }?>(프로필과 배너에 출력되니 항상 최신번호로 유지해주세요) &nbsp;&nbsp;&nbsp;<!-- <a onclick="window.open('//localhost/gnuboard/bbs/member_hp_change.php?mb_id=juho3141','전화번호 변경요청','width=300,height=300,scrollbars=no,padding=0, margin=0, top=300,left=800');" style="color:#000;background-color:#efefef; padding:5px; border:1px solid #696969; border-radius:5px; text-decoration:none">
                                     <i class="fa fa-address-book-o"></i><span>&nbsp;전화번호 변경요청</span></a>&nbsp;&nbsp;&nbsp;<font color="red"><b>※ 전화번호는 운영자가 확인 후 변경처리됩니다.</b></font> --></dd> 
-                <dt>가입일</dt><dd><?php echo $row['mb_datetime'];?></dd>
+                <!-- <dt>가입일</dt><dd><?php echo $row['mb_datetime'];?></dd>
                 <dt>최근 로그인</dt><dd><?php echo $row['mb_today_login'];?></dd>                
-                <dt>서명</dt><dd style="color:#ccc">…</dd>
+                <dt>서명</dt><dd style="color:#ccc">…</dd> -->
         </dl>
     </section>
 </div>
