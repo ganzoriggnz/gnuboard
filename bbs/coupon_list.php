4b00008821afbf2910f542ef7d5400aa6d694c7b
Nurbyek <nuugii.k@gmail.com>
2021-01-12 17:56:52 +0800

    Update

--- bbs/coupon_list.php
+++ bbs/coupon_list.php
@@ -95,12 +95,12 @@ $res2 = sql_query($sql2);
 while($row2 = sql_fetch_array($res2)){
     $sql3 = "SELECT * FROM $g5[coupon_sent_table] WHERE cos_accept='Y' AND cos_nick = '{$row2['cos_nick']}' AND cos_entity = '{$row2['cos_entity']}'";
     $res3 = sql_fetch($sql3);
-    $sql4 = "SELECT * FROM $g5[coupon_alert_table] WHERE cos_nick = '{$row2['wr_name']}' AND cos_entity = '{$row2['wr_7']}'";
+    $sql4 = "SELECT COUNT(alt_no) as alt_cnt FROM $g5[coupon_alert_table] WHERE cos_nick = '{$row2['cos_nick']}' AND cos_entity = '{$row2['cos_nick']}'";
     $res4 = sql_fetch($sql4); 
     $sql7 = "SELECT * FROM $re_table WHERE wr_name = '{$row2['cos_nick']}' AND wr_7 = '{$row2['cos_entity']}'";
     $res7 = sql_fetch($sql7);
     $now = G5_TIME_YMDHIS;
-    if($row2['cos_entity'] !== $res7['wr_7'] && !$res4['alt_no'] && ($now > $row2['cos_post_datetime'])){
+    if($row2['cos_entity'] !== $res7['wr_7'] && $res4['alt_cnt'] == 0 && ($now > $row2['cos_post_datetime'])){
         $sql4 = "INSERT INTO $g5[coupon_alert_table] 
                         SET cos_nick = '{$row2['cos_nick']}',
                             cos_entity = '{$row2['cos_entity']}',
@@ -113,11 +113,10 @@ while($row2 = sql_fetch_array($res2)){
 
             $sql5 = "UPDATE $g5[coupon_sent_table] 
                         SET cos_alt_quantity = '{$res3['cos_alt_quantity']}' + 1
-                        WHERE cos_accept='Y' AND cos_nick = '{$row2['wr_name']}' AND cos_entity = '{$row2['wr_7']}'";
-            echo $sql5;
+                        WHERE cos_accept='Y' AND cos_nick = '{$row2['cos_nick']}' AND cos_entity = '{$row2['cos_entity']}'";
             sql_query($sql5);          
     } 
-    if($row2['wr_id'] && !$res4['alt_no'] && ($row2['wr_datetime'] > $res3['cos_post_datetime'])){
+    /* if($row2['wr_id'] && !$res4['alt_no'] && ($row2['wr_datetime'] > $res3['cos_post_datetime'])){
             $sql5 = "INSERT INTO $g5[coupon_alert_table] 
                         SET cos_nick = '{$row2['cos_nick']}',
                             cos_entity = '{$row2['cos_entity']}',
@@ -133,7 +132,7 @@ while($row2 = sql_fetch_array($res2)){
                         WHERE cos_accept='Y' AND cos_nick = '{$row2['cos_nick']}' AND cos_entity = '{$row2['cos_entity']}'";
 
             sql_query($sql6);                   
-    }
+    } */
 } 
 
 $coupon_list_skin_path = get_skin_path('coupon', 'NB-Basic');
