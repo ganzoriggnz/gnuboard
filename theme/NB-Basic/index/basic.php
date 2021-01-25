33d90f96234ce65356ac876570321e7560ab2a5c
Ganzorig Baatar <ganzoriggnz@gmail.com>
2021-01-25 10:18:48 +0800

    chat size fixed

--- theme/NB-Basic/index/basic.php
+++ theme/NB-Basic/index/basic.php
@@ -21,73 +21,6 @@ if ($nt_wing_path)
 
         <!-- 메인 영역 -->
         <div class="col-md-9<?php echo ($is_left_side) ? ' order-md-2' : ''; ?> na-col flex80d"  style=""  >
-            <!--                                                                                               -->
-            <!--			<div class="mb-4">-->
-            <!--				--><?php //echo na_widget('basic-title', 'title-1', 'xl=27%', 'auto=0'); //타이틀
-                                    ?>
-            <!--			</div>-->
-            <!---->
-            <!--			<div class="row na-row">-->
-            <!--				<div class="col-md-4 na-col">-->
-            <!---->
-            <!-- 위젯 시작 { -->
-            <!--					<h3 class="h3 f-lg en">-->
-            <!--						<a href="--><?php //echo get_pretty_url('video');
-                                                    ?>
-            <!--">-->
-            <!--							<span class="float-right more-plus"></span>-->
-            <!--							게시판-->
-            <!--						</a>-->
-            <!--					</h3>-->
-            <!--					<hr class="hr"/>-->
-            <!--					<div class="mt-3 mb-4">-->
-            <!--						--><?php //echo na_widget('basic-wr-list', 'tlist-1', 'bo_list=video ca_list=게임 rank=red');
-                                            ?>
-            <!--					</div>-->
-            <!-- } 위젯 끝-->
-            <!---->
-            <!--				</div>-->
-            <!--				<div class="col-md-4 na-col">-->
-            <!---->
-            <!-- 위젯 시작 { -->
-            <!--					<h3 class="h3 f-lg en">-->
-            <!--						<a href="--><?php //echo get_pretty_url('video');
-                                                    ?>
-            <!--">-->
-            <!--							<span class="float-right more-plus"></span>-->
-            <!--							게시판-->
-            <!--						</a>-->
-            <!--					</h3>-->
-            <!--					<hr class="hr"/>-->
-            <!--					<div class="mt-3 mb-4">-->
-            <!--						--><?php //echo na_widget('basic-wr-list', 'tlist-2', 'bo_list=video ca_list=게임 rank=green');
-                                            ?>
-            <!--					</div>-->
-            <!-- } 위젯 끝-->
-            <!---->
-            <!--				</div>-->
-            <!--				<div class="col-md-4 na-col">-->
-            <!---->
-            <!-- 위젯 시작 { -->
-            <!--					<h3 class="h3 f-lg en">-->
-            <!--						<a href="--><?php //echo get_pretty_url('video');
-                                                    ?>
-            <!--">-->
-            <!--							<span class="float-right more-plus"></span>-->
-            <!--							게시판-->
-            <!--						</a>-->
-            <!--					</h3>-->
-            <!--					<hr class="hr"/>-->
-            <!--					<div class="mt-3 mb-4">-->
-            <!--						--><?php //echo na_widget('basic-wr-list', 'tlist-3', 'bo_list=video ca_list=게임 rank=blue');
-                                            ?>
-            <!--					</div>-->
-            <!-- } 위젯 끝-->
-            <!---->
-            <!--				</div>-->
-            <!--			</div>-->
-            <!---->
-            <!-- 위젯 시작 { -->
 
             <?php include_once('./_common.php');
 
@@ -146,98 +79,12 @@ if ($nt_wing_path)
             <!-- hulan nemsen -->
 
                 <div style="position: fixed; bottom:0px; right:10px; z-index:9999">
-		<a onclick="window.open('/bbs/chat.php','채팅방참여','width=500,height=550,scrollbars=yes,top=10,left=100'); ">
+		<a onclick="window.open('/bbs/chat.php','채팅방참여','width=520,height=520,scrollbars=yes,top=10,left=100'); ">
 		<img src="<?php echo G5_URL?>/img/chat.png" title=""></a>
 
             </div>
-
-
-
-            <!---->
-            <!---->
-            <!--			<hr class="hr"/>-->
-            <!--			<div class="px-3 px-sm-0 my-3">-->
-            <!--				--><?php //echo na_widget('basic-wr-gallery', 'gallery-1', 'bo_list=video ca_list=게임 rows=8');
-                                    ?>
-            <!--			</div>-->
-            <!-- } 위젯 끝-->
-
-
-            <!--			<div class="row na-row">-->
-            <!--				<div class="col-md-4 na-col">-->
-
-            <!-- 위젯 시작 { -->
-            <!--					<h3 class="h3 f-lg en">-->
-            <!--						<a href="--><?php //echo get_pretty_url('video');
-                                                    ?>
-            <!--">-->
-            <!--							<span class="float-right more-plus"></span>-->
-            <!--							게시판-->
-            <!--						</a>-->
-            <!--					</h3>-->
-            <!--					<hr class="hr"/>-->
-            <!--					<div class="mt-3 mb-4">-->
-            <!--						--><?php //echo na_widget('basic-wr-list', 'blist-1', 'bo_list=video ca_list=게임');
-                                            ?>
-            <!--					</div>-->
-            <!-- } 위젯 끝-->
-
-            <!--				</div>-->
-            <!--				<div class="col-md-4 na-col">-->
-
-            <!-- 위젯 시작 { -->
-            <!--					<h3 class="h3 f-lg en">-->
-            <!--						<a href="--><?php //echo get_pretty_url('video');
-                                                    ?>
-            <!--">-->
-            <!--							<span class="float-right more-plus"></span>-->
-            <!--							게시판-->
-            <!--						</a>-->
-            <!--					</h3>-->
-            <!--					<hr class="hr"/>-->
-            <!--					<div class="mt-3 mb-4">-->
-            <!--						--><?php //echo na_widget('basic-wr-list', 'blist-2', 'bo_list=video ca_list=게임');
-                                            ?>
-            <!--					</div>-->
-            <!-- } 위젯 끝-->
-
-            <!--				</div>-->
-            <!--				<div class="col-md-4 na-col">-->
-
-            <!-- 위젯 시작 { -->
-            <!--					<h3 class="h3 f-lg en">-->
-            <!--						<a href="--><?php //echo get_pretty_url('video');
-                                                    ?>
-            <!--">-->
-            <!--							<span class="float-right more-plus"></span>-->
-            <!--							게시판-->
-            <!--						</a>-->
-            <!--					</h3>-->
-            <!--					<hr class="hr"/>-->
-            <!--					<div class="mt-3 mb-4">-->
-            <!--						--><?php //echo na_widget('basic-wr-list', 'blist-3', 'bo_list=video ca_list=게임');
-                                            ?>
-            <!--					</div>-->
-            <!-- } 위젯 끝-->
-
         </div>
     </div>
-
-    <!-- 위젯 시작 { -->
-    <!--			<h3 class="h3 f-lg en">-->
-    <!--				<a href="--><?php //echo get_pretty_url('video');
-                                    ?>
-    <!--">-->
-    <!--					<span class="float-right more-plus"></span>-->
-    <!--					배너-->
-    <!--				</a>-->
-    <!--			</h3>-->
-    <!--			<hr class="hr"/>-->
-    <!--			<div class="px-3 px-sm-0 mt-3 mb-4">-->
-    <!--				--><?php //echo na_widget('basic-banner', 'banner-1');
-                            ?>
-    <!--			</div>-->
-    <!-- } 위젯 끝-->
 
 </div>
 <!-- 사이드 영역 -->
