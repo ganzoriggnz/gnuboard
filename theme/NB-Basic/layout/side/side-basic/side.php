f5c7fbcc5e6aff34db9ca8ab2bacfbbbfa417b4e
Nurbyek <nuugii.k@gmail.com>
2021-01-28 16:58:50 +0800

    Update
    Update

--- theme/NB-Basic/layout/side/side-basic/side.php
+++ theme/NB-Basic/layout/side/side-basic/side.php
@@ -4,7 +4,77 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 add_stylesheet('<link rel="stylesheet" href="'.$nt_side_url.'/side.css">', 10);
 ?>
 
+<?php if($is_index) { // 인덱스에서만 출력 ?>
+	<!-- 로그인 시작 -->
 
+	<!-- 로그인 끝 -->
+<?php } else { // 페이지에서는 메뉴 출력 ?>
+	<?php
+	$mes = array();
+	for ($i=0; $i < $menu_cnt; $i++) {
+		// 주메뉴 이하 사이트이고 서브메뉴가 있으면...
+		if($menu[$i]['on']) {
+			$mes = $menu[$i];
+			break;
+		}
+    }
+
+	// 선택메뉴가 있다면...
+	if(!empty($mes)) {
+    ?>
+		<div id="nt_side_menu" class="font-weight-normal mb-4" style="display: none;">
+			<div class="bg-primary text-white text-center p-4 py-sm-5 en">
+				<h4>
+					<i class="fa <?php echo $mes['icon'] ?>" aria-hidden="true"></i>
+					<?php echo $mes['text'];?>
+				</h4>
+			</div>
+			<?php if(isset($mes['s'])) { ?>
+				<ul class="me-ul border border-top-0">
+				<?php for ($i=0; $i < count($mes['s']); $i++) {
+					$me = $mes['s'][$i];
+				?>
+				<li class="me-li">
+					<?php if(isset($me['s'])) { //Is Sub Menu ?>
+						<i class="fa fa-caret-down tree-toggle me-i"></i>
+					<?php } ?>
+					<a class="me-a" href="<?php echo $me['href'];?>" target="<?php echo $me['target'];?>">
+						<i class="fa <?php echo $me['icon'] ?> fa-fw" aria-hidden="true"></i>
+						<?php echo $me['text'];?>
+					</a>
+
+					<?php if(isset($me['s'])) { //Is Sub Menu ?>
+						<ul class="me-ul1 tree <?php echo ($me['on']) ? 'on' : 'off'; ?>">
+						<?php for($j=0; $j < count($me['s']); $j++) {
+								$me1 = $me['s'][$j];
+						?>
+							<?php if($me1['line']) { //구분라인 ?>
+								<li class="me-line1"><a class="me-a1"><?php echo $me1['line'];?></a></li>
+							<?php } ?>
+
+							<li class="me-li1<?php echo ($me1['on']) ? ' active' : ''; ?>">
+								<a class="me-a1" href="<?php echo $me1['href'];?>" target="<?php echo $me1['target'];?>">
+									<i class="fa <?php echo $me1['icon'] ?> fa-fw" aria-hidden="true"></i>
+									<?php echo $me1['text'];?>
+								</a>
+							</li>
+						<?php } //for ?>
+						</ul>
+					<?php } //is_sub ?>
+				</li>
+				<?php } //for ?>
+				</ul>
+			<?php } //is_sub ?>
+		</div>
+		<script>
+		$(document).ready(function () {
+			$(document).on('click', '#nt_side_menu .tree-toggle', function () {
+				$(this).parent().children('ul.tree').toggle(200);
+			});
+		});
+		</script> 
+	<?php } ?>
+<?php } ?>
 
 <!-- hulan nemsen  -->
 <div class="side_cate">
