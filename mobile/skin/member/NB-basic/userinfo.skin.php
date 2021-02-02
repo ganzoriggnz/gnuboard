d77adc561459ea8c0a777d8959c24d0a1b3b899b
Ganzorig Baatar <ganzoriggnz@gmail.com>
2021-02-02 20:00:24 +0800

    user info mobile

--- mobile/skin/member/NB-basic/userinfo.skin.php
+++ mobile/skin/member/NB-basic/userinfo.skin.php
@@ -3,7 +3,6 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
-
 ?>
 <div id="bo_v">
     <nav id="user_cate" class="sly-tab font-weight-normal mb-2">
@@ -168,7 +167,6 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 				<td class="cl_tr" style="text-align: left;"> 
                 <?php echo $today_login;?></td>	
         </tr>
-
          <!-- user 서명     -->
         <tr style=" font-size: 12px">
 				<td class="cl_tr"><label class="col-form-label" for="reg_mb_nick">서명</label></td>
