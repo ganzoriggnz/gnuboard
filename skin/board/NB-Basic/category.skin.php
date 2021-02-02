d124f72d8129d4b1d5968ca2265be9679bf700f1
Ganzorig Baatar <ganzoriggnz@gmail.com>
2021-02-02 17:44:24 +0800

    size changed

--- skin/board/NB-Basic/category.skin.php
+++ skin/board/NB-Basic/category.skin.php
@@ -28,14 +28,14 @@ for ($i=0; $i<$ca_count; $i++) {
 #bo_cate ul {zoom:1}
 #bo_cate ul:after {display:block;visibility:hidden;clear:both;content:"";}
 #bo_cate li {display:inline-block;padding:0px;  }
-#bo_cate a {border-radius: 2px; font-size:12px; color:#000; width:100%; height:39px; display: flex; align-items:center; justify-content:center; margin-left: 0px;}
+#bo_cate a {border-radius: 2px; font-size:12px; color:#000; width:95px; height:39px; display: flex; align-items:center; justify-content:center; margin-left: 0px;}
 #bo_cate a:focus, #bo_cate a:hover, #bo_cate a:active { text-decoration:none; color:#000;font-weight: bold; background-color: #fff }
 #bo_cate #bo_cate_on {z-index:2; font-size:12px; font-weight: bold; color:#000; width:95px; height:39px; display:flex; align-items:center; justify-content: center;}
 
 </style>
 <nav id="bo_cate" class="sly-tab font-weight-normal mb-2">
 	<h3 class="sr-only"><?php echo $board['bo_subject'] ?> 분류 목록</h3>
-		<div class="bo_cate pl-2">
+		<div class="bo_cate pl-2 cols-xl-5">
 			<div class="bo_cate">
 				<ul class="bo_cate_ul">
 					<?php echo $category_option ?>
