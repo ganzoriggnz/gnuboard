3418b80f588638fe4d3f59eb9a4cd9d8aa89f418
Ganzorig Baatar <ganzoriggnz@gmail.com>
2021-02-02 10:05:07 +0800

    myreview deer active iig n yanzlaw

--- skin/member/NB-Basic/myreview.skin.php
+++ skin/member/NB-Basic/myreview.skin.php
@@ -80,7 +80,7 @@ add_stylesheet('<link rel="stylesheet" href="' . $member_skin_url . '/style.css"
                     <?php } ?>
                     <!-- if nuhtsul hulan nemsen 후기는 업소레벨에만 있으면 된다 -->
                     <?php if ($member['mb_level'] == 26 || $member['mb_level'] == 27) { ?>
-                    <li>
+                    <li class="active">
                         <a class="py2 px-3" href="<?php echo G5_BBS_URL ?>/myreview.php">
                             <span>
                                 <i class="fa fa-pencil-alt">
