96c457c9fa3c5f5b45fa7f67a04948a8e98502be
Ganzorig Baatar <ganzoriggnz@gmail.com>
2021-02-02 21:28:50 +0800

    member search mobile , scrap mobile tab changedadd

--- mobile/skin/member/NB-basic/scrap.skin.php
+++ mobile/skin/member/NB-basic/scrap.skin.php
@@ -4,6 +4,100 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
 ?>
+<div id="bo_v">
+    <nav id="user_cate" class="sly-tab font-weight-normal mb-2">
+		<div class="px-3 px-sm-0">
+			<div class="d-flex">
+				<div id="user_cate_list" class="sly-wrap flex-grow-1">
+					<ul id="user_cate_ul" class="sly-list d-flex border-left-0">
+						<li >
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/userinfo.php" >
+                                <span>
+                                <i class="fa fa-user">
+                                회원정보
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/mypost.php">
+                                <span>
+                                <i class="fa fa-pencil-alt">
+                                내 글
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="py2 px-3" href="<?php echo G5_BBS_URL ?>/point2.php">
+                                <span>
+                                <i class="fa fa-book">
+                                파편조각 : <b><?php echo number_format($member['mb_point2']);?></b>
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        </ul>
+						<ul id="user_cate_ul" class="sly-list d-flex">
+                        <li>
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/point.php">
+                                <span>
+                                <i class="fa fa-gem">
+                                파운드 : <b><?php echo number_format($member['mb_point']);?></b>
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <li class="active">
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/scrap.php">
+                                <span>
+                                <i class="fa fa-paperclip">
+                                스크랩
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <?php if ($member['mb_level'] == 27) { ?>
+                        <li>
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/coupon_create.php">
+                                <span>
+                                <i class="fa fa-cubes">
+                                쿠폰지원
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <?php } ?>
+                        <?php if ($member['mb_level'] < 23) { ?>
+                        <li>
+                            <a class="py2 px-3" href= "<?php echo G5_BBS_URL ?>/coupon_accept.php">
+                                <span>
+                                <i class="fa fa-handshake">
+                                쿠폰관리
+                                </i>
+                                </span>
+                            </a>
+                        </li>
+                        <?php } ?>
+                        <!-- if nuhtsul hulan nemsen 후기는 업소레벨에만 있으면 된다 -->
+                        <?php if ($member['mb_level'] == 26 || $member['mb_level'] == 27) { ?>
+                        <li>
+                            <a class="py2 px-3" href="<?php echo G5_BBS_URL ?>/myreview.php">
+                                <span>
+                                    <i class="fa fa-pencil-alt">
+                                        후기보기
+                                    </i>
+                                </span>
+                            </a>
+                        </li>
+                        <?php } ?>
+                    </ul>
+                    <hr/>
+				</div>
+			</div>
+		</div>
+    </nav>
+
 
 <div id="scrap" class="new_win">
     <h1 id="win_title"><?php echo $g5['title'] ?></h1>
