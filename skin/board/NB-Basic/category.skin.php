fa976f5a8d60bbb7f51ca204b80a063cbd34d0f3
Ganzorig Baatar <ganzoriggnz@gmail.com>
2021-02-02 17:36:30 +0800

    category size width changed

--- skin/board/NB-Basic/category.skin.php
+++ skin/board/NB-Basic/category.skin.php
@@ -28,7 +28,7 @@ for ($i=0; $i<$ca_count; $i++) {
 #bo_cate ul {zoom:1}
 #bo_cate ul:after {display:block;visibility:hidden;clear:both;content:"";}
 #bo_cate li {display:inline-block;padding:0px;  }
-#bo_cate a {border-radius: 2px; font-size:12px; color:#000; width:92px; height:39px; display: flex; align-items:center; justify-content:center; margin-left: 0px;}
+#bo_cate a {border-radius: 2px; font-size:12px; color:#000; width:100%; height:39px; display: flex; align-items:center; justify-content:center; margin-left: 0px;}
 #bo_cate a:focus, #bo_cate a:hover, #bo_cate a:active { text-decoration:none; color:#000;font-weight: bold; background-color: #fff }
 #bo_cate #bo_cate_on {z-index:2; font-size:12px; font-weight: bold; color:#000; width:95px; height:39px; display:flex; align-items:center; justify-content: center;}
 
