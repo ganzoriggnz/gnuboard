7e01424b14902de53f00798a9d0e782969029a95
setgelkhuukhulan <setgelkhuukhulan@gmail.com>
2021-02-07 21:55:23 +0800

    삭제

--- skin/board/NB-Basic/view.skin.php
+++ skin/board/NB-Basic/view.skin.php
@@ -180,12 +180,12 @@ $view_subject = get_text($view['wr_subject']);
 									</a>
 								<?php } ?>
 								<!-- hulan nemsen level 24.25 can not delete post  -->
-								<?php if ($delete_href && $member['mb_level'] != 24 && $member['mb_level'] != 25) { ?>
+								<?php if ($delete_href && $member['mb_level'] != 24 && $member['mb_level'] != 25) {if($gr_id !="attendance" || $is_admin ) {  ?>
 									<a href="<?php echo $delete_href ?>" onclick="del(this.href); return false;" class="btn btn-primary py-2" role="button">
 										<i class="fa fa-trash-o fa-fw" aria-hidden="true"></i>
 										글삭제
 									</a>
-								<?php } ?>
+								<?php }} ?>
 								<?php if ($copy_href) { ?>
 									<a href="<?php echo $copy_href ?>" onclick="board_move(this.href); return false;" class="btn btn-primary py-2" role="button">
 										<i class="fa fa-files-o fa-fw" aria-hidden="true"></i>
