b6edd85ce25439f245a87116fea54fc52427ab6e
Ganzorig Baatar <ganzoriggnz@gmail.com>
2021-02-16 17:08:58 +0800

    48px zai awsan

--- theme/NB-Basic/head.php
+++ theme/NB-Basic/head.php
@@ -165,7 +165,7 @@ if($nt_top_path)
         <?php if($is_page_col) { ?>
         <div class="nt-container py-3 py-sm-4">
             <?php if($is_page_col == "two") { ?>
-            <div class="row na-row">
+            <div class="row na-row"  style="display: flex; justify-content: space-between;">
                 <div class="col-md-<?php echo $is_content_col ?>
 				<?php echo ($tset['left_side']) ? ' order-md-2' : ' order-md-2';?> na-col">
                     <?php } ?>
