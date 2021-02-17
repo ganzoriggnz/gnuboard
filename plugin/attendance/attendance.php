c83bcc8597549af8985549283492abb86d3ce135
Nurbyek <nuugii.k@gmail.com>
2021-02-17 12:39:51 +0800

    attendance text input, submit button design change
    attendance text input, submit button design change

--- plugin/attendance/attendance.php
+++ plugin/attendance/attendance.php
@@ -100,6 +100,7 @@ if ($d) {
 }
 ?>
 <link rel="stylesheet" href="<?php echo $G5_URL ?>/plugin/attendance/attendance.css" />
+
 <div id="bo_v">
 <table width="100%" cellpadding="0" cellspacing="0" border="0">
 <tr height="<?php echo $config['cf_home_height']?>"><td align="center" colspan="<?php echo $colspan?>"></td></tr>
@@ -335,30 +336,52 @@ function dateGo(day)
 <tr><td align="center" colspan="<?php echo $colspan?>"></td></tr>
 <tr>
     <td style="height:32px; border:0px solid">
-<form name="fattendance" method="post" onsubmit="return fattendance_submit(this);" style="margin:0px;">
-<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0"><tr><td>
+<form id="fattendance" name="fattendance" action = "<?php echo G5_PLUGIN_URL ?>/attendance/attendance_write_update.php" method="post" enctype="multipart/form-data" style="margin:0px;">
+<!-- <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0"><tr><td> 
     <div class=" alert bg-light border form-row" style="height:40px; text-align: center; align-items: center; justify-content: center; margin:0px;">
             <input type="text" id="subject" name="subject" size="50" style="width:50%;" class="form-control input" value="출석인사를 입력해 주세요." onmouseover="if(!this.value || this.value == '출석인사를 입력해 주세요.')this.value='';" > 
             <input type="image" src= "<?php echo G5_IMG_URL ?>/attendance_ok.gif" border="0"  align="absmiddle">
+    </div> 
+    </td></tr></table> -->
+    <div id="msg_content" class="msg-content">
+        <div class="msg-cell">
+            <textarea id="subject" name="subject" class="form-control input-sm" rows="4" required="" maxlength="65536">출석인사를 입력해 주세요.</textarea>
+        </div>
+        <div tabindex="14" class="msg-cell msg-submit" onclick="att_submit();">
+            출석하기
+        </div>
     </div>
-</td></tr></table>
 </form>
 <script type="text/javascript"> 
-
-function fattendance_submit(f) 
+/* function fattendance_submit(f) 
 { 
 
     var ChkSubject = document.getElementById("subject").value; 
+
     if (!ChkSubject || ChkSubject == '출석인사를 입력해 주세요.') { 
 
         alert("출석인사를 입력하세요."); 
         return false; 
 
     } 
+   f.action = "<?php echo G5_PLUGIN_URL ?>/attendance/m_attendance_write_update.php"; 
+    f.action = "./m_attendance_write_update.php"; 
+}  */
 
-    f.action = "<?php echo G5_PLUGIN_URL ?>/attendance/attendance_write_update.php"; 
-    // f.action = "./attendance_write_update.php"; 
-} 
+    function att_submit()
+    { 
+        debugger;
+        var ChkSubject = $('#subject').val();
+        if (!ChkSubject || ChkSubject == '출석인사를 입력해 주세요.') { 
+
+            alert("출석인사를 입력하세요."); 
+            return false; 
+
+        } 
+        else {
+            $('#fattendance').submit();
+        }
+    } 
 </script>
     </td>
 </tr>
