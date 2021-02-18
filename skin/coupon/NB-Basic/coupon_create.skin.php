0fa4f96f9971d20338307f1f9df5150d0d952734
Ganzorig Baatar <ganzoriggnz@gmail.com>
2021-02-18 17:16:00 +0800

    coupon changed css

--- skin/coupon/NB-Basic/coupon_create.skin.php
+++ skin/coupon/NB-Basic/coupon_create.skin.php
@@ -17,7 +17,6 @@ add_stylesheet('<link rel="stylesheet" href="'.$coupon_create_skin_url.'/style.c
                                 <span>
                                 <img src="<?php echo G5_URL?>/img/solid/user.svg" class="svg-img" style="height :13px;" >&nbsp
                                 회원정보
-                             
                                 </span>
                             </a>
                         </li>
@@ -93,15 +92,83 @@ add_stylesheet('<link rel="stylesheet" href="'.$coupon_create_skin_url.'/style.c
     </nav>
     <section class="xm">
     <div class="couponbg" >
-        <div class="coupon_noti">이번달 쿠폰개수는 1일 -5일 까지 수정 가능합니다.<br>이번달 잔여 쿠폰개수는 수정할 수 없습니다.
-    </div>
-      
+        <div class="coupon_noti">이번달 쿠폰개수는 1일 -5일 까지 수정 가능합니다.<br>이번달 잔여 쿠폰개수는 수정할 수 없습니다.</div>
+
+    <div class="coupon_info">
+    <form id="fcouponcreate" name="fcouponcreate" action="<?php echo $coupon_action_url ?>" onsubmit="return fcouponcreate_submit(this);" method="post" enctype="multipart/form-data" autocomplete="off">
+                <input type="hidden" name="mb_id" value="<?php echo $member['mb_id'] ?>">
+                <input type="hidden" name="co_entity" value="<?php echo $member['mb_name'] ?>">
+            <table class="coupon-table">
+                <thead>
+                    <tr class="coupon-create-tr">
+                        <th class="coupon-create-th"></th>
+                        <th class="coupon-create-th"> 쿠폰 지원 개수</th>
+                        <th class="coupon-create-th">이번달 쿠폰 지원 개수</th>
+                        <th class="coupon-create-th">이번달 잔여 쿠폰 개수</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="coupon-create-tr">
+                        <td class="coupon_label col-form-label">원가권 :</td>
+                        <td class="coupon-create-td"><input type="number" name="co_sale_num" value="<?php echo $row['co_sale_num']; ?>" placeholder="" class="form-control coupon_input" <?php if($co_created_datetime > $co_insert_date) { echo 'disabled="disabled"';}  else {echo '';}?>></td>
+
+                        <td class="coupon-create-td" style="border-right: 1px solid #ced4da; text-align: center;"><?php echo $row['co_sent_snum']; ?>0</td>
+                        <td class="coupon-create-td"><?php echo $diff_s; ?></td>
+                    </tr>
+
+                    <tr class="coupon-create-tr">
+                        <td class="coupon_label col-form-label">무료권 :</td>
+                        <td class="coupon-create-td" > <input type="number" name="co_free_num" value="<?php echo $row['co_free_num']; ?>" placeholder="" class="form-control coupon_input" <?php if($co_created_datetime > $co_insert_date) { echo 'disabled="disabled"';}  else { echo '';}?>></td>
+
+                        <td class="coupon-create-td" style="border-right: 1px solid #ced4da; text-align: center; padding-top: 10px;"><?php echo $row['co_sent_fnum']; ?>0</td>
+                        <td class="coupon-create-td" style="text-align: center; padding-top: 10px;"><?php echo $diff_f; ?></td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="p-20">
+                    <div class="coupon_label"></div>
+                    <button type="submit" id="btn_submit" accesskey="s" <?php if($co_created_datetime > $co_insert_date) { echo 'class="miss_but_3" disabled="disabled"';}  else { echo 'class="miss_but_1"';}?>>저장</button>
+                </div>
+            <div class="popup_box1">
+                    <h1>쿠폰</h1>
+                    <label>수정은 매월 1일부터 3일까지만 가능합니다.</label>
+                    <div class="btns1">
+                        <a href="#" class="btn1">확인</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+            <script>
+
+            function fcouponcreate_submit(f) {
+                    if (!f.co_sale_num) {
+                        alert("Please insert quantity of sale coupon!");
+                        f.co_sale_num.focus();
+                        return false;
+                    } 
+
+                    if (!f.co_free_num) {
+                        alert("Please insert quantity of free coupon!");
+                        f.co_free_num.focus();
+                        return false;
+                    }
+
+                    var agree=confirm("쿠폰 개수를 저장하시겠습니까?");
+                    if(agree)
+                        return true;
+                    else 
+                        return false;                                       
+            }
+            </script>
+
+
+
+<!-- 
         <div class="coupon_info" style="float-left">
             <h6>이번달 쿠폰 지원 개수</h6>
             <form id="fcouponcreate" name="fcouponcreate" action="<?php echo $coupon_action_url ?>" onsubmit="return fcouponcreate_submit(this);" method="post" enctype="multipart/form-data" autocomplete="off">
                 <input type="hidden" name="mb_id" value="<?php echo $member['mb_id'] ?>">
                 <input type="hidden" name="co_entity" value="<?php echo $member['mb_name'] ?>">
-
                 <div class="p-20">
                     <div class="coupon_label col-form-label">원가권 :</div>
                     <input type="number" name="co_sale_num" value="<?php echo $row['co_sale_num']; ?>" placeholder="" class="form-control coupon_input" <?php if($co_created_datetime > $co_insert_date) { echo 'disabled="disabled"';}  else {echo '';}?>>
@@ -144,10 +211,10 @@ add_stylesheet('<link rel="stylesheet" href="'.$coupon_create_skin_url.'/style.c
                     else 
                         return false;                                       
             }
-
             </script>
-        </div>
-        <div class="coupon_current">
+        </div> -->
+
+        <!-- <div class="coupon_current">
             <table class="coupon-table">
                 <thead>
                     <tr class="coupon-create-tr">
@@ -166,9 +233,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$coupon_create_skin_url.'/style.c
                     </tr>
                 </tbody>
             </table>
-        </div>
-    </div>  
-        </section>
+        </div> -->
+        <!-- </div>   -->
+    </section>
 </div> 
-  
-
