d534d64ed81c9e3e5c4c35a46f88a031644e87fb
byambaa <tsoogon@gmail.com>
2021-02-18 23:16:21 +0800

    Update write.skin.php

--- skin/board/NB-Basic/write.skin.php
+++ skin/board/NB-Basic/write.skin.php
@@ -303,7 +303,7 @@ if ($is_member)
 				</li>
 			<?php } ?>
 
-			<?php if (($is_admin || $is_member || ($gr_id == 'attendance'  &&  $w != 'u')) && $fr !='widget') { ?> <!---fr- "from" ali files duudsaniig zaasan -->
+			<?php if (($is_admin || $is_member || ($gr_id == 'attendance'  &&  $w != 'u')) && $fr !='widget' && $member['mb_27'] != $bo_table ) { ?> <!---fr- "from" ali files duudsaniig zaasan -->
 				
 				<?php if ($is_category || (($member['mb_7'] && $member['mb_6']==$bo_table) &&  $w != 'u')) { ?>
 					<li class="list-group-item">
@@ -317,22 +317,6 @@ if ($is_member)
 							</div>
 						</div>
 					</li>
-				<?php } ?>
-			<?php } ?>
-			<?php if (($is_admin || $is_member || ($gr_id == 'attendance'  &&  $w != 'u')) && $fr =='widget'&& $member['mb_27'] == $bo_table ) { ?>
-				
-				<?php if ($is_category || (($member['mb_7'] && $member['mb_6']==$bo_table) &&  $w != 'u')) { ?>
-				<!--	<li class="list-group-item">
-						<div class="form-group row mb-0">
-							<label class="col-md-2 col-form-label">분류<strong class="sr-only">필수</strong></label>
-							<div class="col-md-4">
-								<select name="ca_name" id="ca_name" required class="custom-select" <?php if ($member['mb_7'] && $member['mb_6']==$bo_table) echo "disabled" ?> >
-									<option value="">선택하세요</option>
-									<?php echo $category_option ?>
-								</select>
-							</div>
-						</div>
-					</li> -->
 				<?php } ?>
 			<?php } ?>
 
