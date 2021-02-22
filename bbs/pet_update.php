976443a182a33ff3cbe866f58b2ff61cf404e9e6
Nurbyek <nuugii.k@gmail.com>
2021-02-22 12:16:18 +0800

    update pet sql
    update pet sql

--- bbs/pet_update.php
+++ bbs/pet_update.php
@@ -12,32 +12,13 @@ $res = "SELECT * FROM {$g5['pet_table']} WHERE mb_id = '{$member['mb_id']}' AND 
 $row = sql_fetch($res); 
 
 if(!$row['p_no']){
-
-    if($id == 'cat1'){ 
-        $sql1 = "INSERT INTO {$g5['pet_table']} 
-                SET p_but1_datetime = '{$now}',
-                    mb_id = '{$member['mb_id']}'";
-        sql_query($sql1);   
-    } else if($id == 'cat2'){ 
-        $sql2 = "INSERT INTO {$g5['pet_table']} 
-                     SET p_but2_datetime = '{$now}'
-                         mb_id = '{$member['mb_id']}'";
-        sql_query($sql2);
-    
-    } else if($id == 'cat3'){ 
-        $sql3 = "INSERT INTO {$g5['pet_table']} 
-                    SET p_but3_datetime = '{$now}'
-                        mb_id = '{$member['mb_id']}'";
-        sql_query($sql3);   
-    }
+    $sql1 = "INSERT INTO {$g5['pet_table']} 
+            SET p_but1_datetime = '{$now}',
+                mb_id = '{$member['mb_id']}'";
+    sql_query($sql1);   
 }
 else if($row['p_no']){
-    if($id == 'cat1' && $row['p_but1_datetime'] == '0000-00-00 00:00:00'){ 
-        $sql1 = "UPDATE {$g5['pet_table']} 
-                    SET p_but1_datetime = '{$now}',
-                    WHERE p_no = '{$row['p_no']}'";
-        sql_query($sql1);   
-    } else if($id == 'cat2' && $row['p_but2_datetime'] == '0000-00-00 00:00:00'){ 
+    if($id == 'cat2' && $row['p_but2_datetime'] == '0000-00-00 00:00:00'){ 
         $sql2 = "UPDATE {$g5['pet_table']} 
                     SET p_but2_datetime = '{$now}'
                     WHERE p_no = '{$row['p_no']}'";
