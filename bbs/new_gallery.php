f03e61886605a0cd3708af0add9e9552430ca2a9
Nurbyek <nuugii.k@gmail.com>
2021-02-23 09:22:47 +0800

    show also not members
    show also not members

--- bbs/new_gallery.php
+++ bbs/new_gallery.php
@@ -1,7 +1,7 @@
 <?php
 include_once('./_common.php');
-if (!$is_member)
-alert('회원만 조회하실 수 있습니다.', G5_BBS_URL."/login.php?url=".urlencode("{$_SERVER['REQUEST_URI']}"));
+/* if (!$is_member)
+alert('회원만 조회하실 수 있습니다.', G5_BBS_URL."/login.php?url=".urlencode("{$_SERVER['REQUEST_URI']}")); */
 
 include_once('./_head.php');
 
