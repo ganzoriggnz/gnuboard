ad97146e7f3da53f56f2947c276119c3fbdfb9ad
Nurbyek <nuugii.k@gmail.com>
2021-02-23 21:47:34 +0800

    Dugaarlalt davhardal alert nemsen
    Dugaarlalt davhardal alert nemsen

--- skin/member/NB-Basic/phone_change_request.skin.php
+++ skin/member/NB-Basic/phone_change_request.skin.php
@@ -8,7 +8,6 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 
 ?>
 
-
 <div style="background-color:#f0f0f0; color:#000; font-weight:bold; padding:10px 0px 10px 0px; margin:0px; position:absolute; left:0px; top:0px; width:100%;" > <?php echo $g5['title']?></div>
 
 <form name="fmember" id="fmember" method="post" action="<?php echo G5_BBS_URL ?>/member_hp_update.php">
@@ -19,7 +18,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 	<tr><td style="background-color:#f0f0f0;padding:5px">업소명</td><td><?php echo $member['mb_name']?></td></tr>
 	<tr><td style="background-color:#f0f0f0;padding:5px">이전 전화번호</td><td><?php echo $member['mb_hp']?></td></tr>
 	<tr><td style="background-color:#f0f0f0;padding:5px">변경 전화번호</td>
-		<td><input type="text" name="new_hp" id="new_hp" value="<?php if($_POST['new_hp']!="" ) echo $new_hp; else echo $member['mb_10']; ?>"  style="width:120px;height:25px; text-align:left; margin-right:25px; border:1px solid #b5b5b5"  size="14" maxlength="14"></td></tr>
+		<td><input type="text" name="new_hp" id="new_hp" value=""  style="width:120px;height:25px; text-align:left; margin-right:25px; border:1px solid #b5b5b5"  size="14" maxlength="14"></td></tr>
 	</table>
 <br/>
 <div style="text-align:center">
@@ -27,16 +26,15 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 	<button type="button" class="btn btn-primary btn-lg btn-block en" onclick="window.close();">창닫기</button>
 </div>	
 
-<?php if($_POST['new_hp']!='')
+<!-- <?php if($_POST['new_hp']!='')
  echo '<br/><div style="text-align:center">전화번호 변경요청 완료되었습니다</div>'; 
-?>		
+?> -->		
 </div>
 </form>
 <script src="<?php echo NA_URL ?>/js/jquery-3.5.1.min.js"></script>
 <script>
 		$('#change').click(function(){
-			if (confirm("Are you sure to change previous phone number?")) {
-				alert("Your previous number changed successfully");
+			if (confirm("이전 전화 번호를 변경하시겠습니까?")) {
 				$('#fmember').submit();
 			}                  
 		});                                      
--- skin/member/NB-Basic/userinfo.skin.php
+++ skin/member/NB-Basic/userinfo.skin.php
@@ -186,7 +186,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
     </label>
     <div class="col-sm-10 col-form-info">
     <?php if($member['mb_hp']) { echo $member['mb_hp'].'<br>'; }?>(프로필과 배너에 출력되니 항상 최신번호로 유지해주세요)
-					<a onclick="window.open('<?php echo G5_URL ?>/bbs/member_hp_change.php?mb_id=<?php echo $member['mb_id'] ?>','전화번호 변경요청','width=300,height=300,scrollbars=no,padding=0, margin=0, top=300,left=800');" 
+					<a onclick="window.open('<?php echo G5_URL ?>/bbs/member_hp_change.php?mb_id=<?php echo $member['mb_id'] ?>','전화번호 변경요청','width=500,height=300,scrollbars=no,padding=0, margin=0, top=300,left=800');" 
 					style="color:#000;background-color:#efefef; padding:5px; border:1px solid #696969; border-radius:5px; text-decoration:none" >
 						<font style="vertical-align: inherit;">전화번호 변경요청 </font></a>  <label style='color: red;'> &nbsp ※ 전화번호는 운영자가 확인 후 변경처리됩니다.</label>
 								
