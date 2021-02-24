c1ec14a2bbd70dcd56d4dd7cb113fec834112a35
Nurbyek <nuugii.k@gmail.com>
2021-02-24 14:45:44 +0800

    Mobile deerh coupon, coupon niit, new, new niit hoorondin zai zasagdsan
    Mobile deerh coupon, coupon niit, new, new niit hoorondin zai zasagdsan

--- theme/NB-Basic/widget/basic-wr-gallery/widget.rows.php
+++ theme/NB-Basic/widget/basic-wr-gallery/widget.rows.php
@@ -103,7 +103,7 @@ for ($i=0; $i < $list_cnt; $i++) {
             border-radius: 0px;
         }
         </style>
-<li class="col px-2 pb-4" style="width: 230px; height: 300px;">
+<li class="col px-2" style="margin-bottom: 10px;">
     <?php   
         $at = $list[$i]['bo_table'];
         $linkcount = strlen($at) - 2;
