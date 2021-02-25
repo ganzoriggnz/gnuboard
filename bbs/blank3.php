bb83ceaa64f7a8bb7f59b9f77ef69eff69f7cac5
setgelkhuukhulan <setgelkhuukhulan@gmail.com>
2021-02-25 17:17:59 +0800

    메뉴

--- bbs/blank3.php
+++ bbs/blank3.php
@@ -8,7 +8,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 $blank_skin_path = get_skin_path('member', $config['cf_member_skin']);
 $blank_skin_url  = get_skin_url('member', $config['cf_member_skin']);
-$skin_file = $blank_skin_path.'/blank2.skin.php';
+$skin_file = $blank_skin_path.'/blank3.skin.php';
 
 if(is_file($skin_file)) {
     include($skin_file);
