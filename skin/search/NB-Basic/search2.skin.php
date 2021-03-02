ccf623643a846a866fe56ce023dd5153407fa158
Nurbyek <nuugii.k@gmail.com>
2021-03-02 13:16:56 +0800

    Update mobile search gallery cat css
    Update mobile search gallery cat css

--- skin/search/NB-Basic/search2.skin.php
+++ skin/search/NB-Basic/search2.skin.php
@@ -10,7 +10,31 @@ $wset = na_skin_config('search');
 $head_color = ($wset['head_color']) ? $wset['head_color'] : 'primary';
 
 ?>
-<div id="sch_res_detail pt-2" class="mb-4">
+<?php if(G5_IS_MOBILE){ ?>
+	<form name="fsearch" onsubmit="return fsearch_submit(this);" action="<?php echo G5_BBS_URL."/board.php"?>" method="get" class="m-auto" style="width: 100%; height: 50px;">
+		<input type="hidden" name="bo_table" value="gallery">
+		<input type="hidden" name="sca" value="<?php echo $searchd ?>">
+		<input type="hidden" name="subsca" value="<?php echo $searchd ?>">
+
+		<legend class="sound_only">상세검색</legend>
+		<div class="form-row mx-n1">
+			<div class="col-12 pt-2 pt-sm-0 px-1">				
+				<div class="input-group">
+					<input type="text" id="searchd" name="searchd" value="<?php if($searchd) echo $searchd; ?>" required class="form-control"
+					 placeholder="지역 이나 업종 또는 업소명을 입력하세요.">
+					<div class="input-group-append">
+						<button type="submit" class="btn btn-primary" title="검색하기">
+							<i class="fa fa-search" aria-hidden="true"></i>
+							<span class="sr-only">검색하기</span>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		</form>
+
+<?php } else { ?>
+	<div id="sch_res_detail pt-2" class="mb-4">
 	<div class="alert bg-light border p-2 p-sm-3 mb-3 mx-3 mx-sm-0">
 		<form name="fsearch" onsubmit="return fsearch_submit(this);" action="<?php echo G5_BBS_URL."/board.php"?>" method="get" class="m-auto" style="max-width:600px;">
 		<input type="hidden" name="bo_table" value="gallery">
@@ -33,10 +57,14 @@ $head_color = ($wset['head_color']) ? $wset['head_color'] : 'primary';
 			</div>
 		</div>
 		</form>
-		<script>
-		function fsearch_submit(f) {
-			return true;
-		}
-		</script>
+		
 	</div>
 </div>
+
+<?php } ?>
+
+<script>
+	function fsearch_submit(f) {
+		return true;
+	}
+</script>
