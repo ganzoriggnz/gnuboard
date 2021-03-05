026120c4995475b7c63ae39a68c4c1a3199a9009
Nurbyek <nuugii.k@gmail.com>
2021-03-05 09:11:49 +0800

    Change logout and chat location
    Change logout and chat location

--- theme/NB-Basic/layout/sidebar/sidebar-basic/sidebar.php
+++ theme/NB-Basic/layout/sidebar/sidebar-basic/sidebar.php
@@ -51,20 +51,24 @@ $menu_cnt = count($menu);
 			<i class="fa fa-times"></i>
 		</div>
 		<div class="sidebar-wing-icon1">
+			<a class="sidebar-wing-btn" href="<?php echo G5_BBS_URL ?>/logout.php" target="_self" title="주소변경공지">
+				<img src="<?php echo G5_URL?>/img/baseline-input-24px.png" >
+			</a>
 			<a class="sidebar-wing-btn" href="<?php echo G5_URL?>/bbs/chat.php" target="_blank" title="채팅방" >
 				<img src="<?php echo G5_URL?>/img/solid/comment.svg" style="height: 14px;">
 			</a>
-			<a class="sidebar-wing-btn" href="<?php echo G5_BBS_URL ?>/logout.php" target="_self" title="주소변경공지">
-				<img src="<?php echo G5_URL?>/img/baseline-input-24px.png" ></a>
 			<a class="sidebar-wing-btn" href="https://facebook.com" target="_blank" title="페이스 북">
-			<img src="<?php echo G5_URL?>/img/solid/facebook-logo.svg" style="height: 14px;"></a>
+				<img src="<?php echo G5_URL?>/img/solid/facebook-logo.svg" style="height: 14px;">
+			</a>
 			<a class="sidebar-wing-btn" href="https://twitter.com" target="_blank" title="트위터">
-				<img src="<?php echo G5_URL?>/img/twitter.png" ></a>
+				<img src="<?php echo G5_URL?>/img/twitter.png" >
+			</a>
 			<?php if($is_member) { ?>
 				<!-- <a class="sidebar-wing-btn win_memo" href="<?php echo G5_BBS_URL?>/memo_friend.php?kind=friends" target="_blank" title="">
 				<img src="<?php echo G5_URL?>/img/baseline-person-add-24px.png" ></a> -->
 			<a class="sidebar-wing-btn" href="<?php echo G5_URL?>/bbs/member_list.php" target="_self" title="회원검색">
-				<img src="<?php echo G5_URL?>/img/icon_search.png" ></a>
+				<img src="<?php echo G5_URL?>/img/icon_search.png" >
+			</a>
 			<?php } ?>
 		</div>
 	</div>
