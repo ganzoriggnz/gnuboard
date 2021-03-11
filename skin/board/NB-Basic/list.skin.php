a4c8d00106145e4a8ef9ce0fad5abb5d5d5d9f82
Nurbyek <nuugii.k@gmail.com>
2021-03-11 15:18:43 +0800

    Shildegig zarim sambaruudad haragduulahgu bh
    Shildegig zarim sambaruudad haragduulahgu bh

--- skin/board/NB-Basic/list.skin.php
+++ skin/board/NB-Basic/list.skin.php
@@ -54,7 +54,9 @@ add_javascript('<script src="'.G5_JS_URL.'/jquery.rumiTab.js"></script>', 0);
 <!-- 게시판 목록 시작 { -->
 <div id="bo_list_wrap">
 <!-- 인기글 { -->
-	<?php if ($bo_table != "pointrank" && $bo_table != "penyrank" && $bo_table != "levelrank" && $bo_table != "boardadmlist" && $bo_table != "mypage") {
+	<?php if ($bo_table != "pointrank" && $bo_table != "penyrank" && $bo_table != "levelrank" && $bo_table != "boardadmlist" 
+	&& $bo_table != "mypage" && $gr_id!="attendance" && $bo_table != "greeting" && $bo_table != "notice" && $bo_table != "twitter"
+	&& $bo_table != "facebook" && $bo_table != "twitter" && $bo_table != "ucc" && $bo_table != "work_abroad") {
 	if(!G5_IS_MOBILE) include_once('hit_latest.php'); }?>
 	<br>
 	<?php
