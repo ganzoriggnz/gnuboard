e9509148d084ccf17d7621a7910eacc45aae63d5
Nurbyek <nuugii.k@gmail.com>
2021-03-17 13:01:30 +0800

    Footer bichig avaad ondrig ni bagasgah
    Footer bichig avaad ondrig ni bagasgah

--- theme/NB-Basic/layout/footer/footer-basic/footer.php
+++ theme/NB-Basic/layout/footer/footer-basic/footer.php
@@ -21,9 +21,9 @@ add_stylesheet('<link rel="stylesheet" href="'.$nt_footer_url.'/footer.css">', 0
 <?php } ?>
 <footer id="nt_footer" class="f-de font-weight-normal">
 	<div class="nt-container">
-		<div class="item-text">
+		<!-- <div class="item-text">
 			본 사이트에서는 회원들간 자료공유에 따른 중계역할만 하며, 저작권에 대한 책임은 업로드를 한 회원에게 있습니다. 또한 해당 자료에 대한 원본파일은 서버에 보관하지 않습니다. <br>
 			각각의 게시물에 대한 내용은 본 사이트에서 보증하지 않으며, 유해하다고 판단될시 관리자에 의해 삭제조치 될 수 있습니다. 사이트 이용에 따라 자료에 대한 판단 책임은 이용자에게 있습니다.
-		</div>
+		</div> -->
 	</div>
 </footer>
