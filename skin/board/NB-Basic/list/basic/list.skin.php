c2370ef4fc874a4001b3fbb002bef7b1b8a31ed6
Ganzorig Baatar <ganzoriggnz@gmail.com>
2021-03-18 15:53:23 +0800

    mobile board list add notice num

--- skin/board/NB-Basic/list/basic/list.skin.php
+++ skin/board/NB-Basic/list/basic/list.skin.php
@@ -29,56 +29,64 @@ $list_cnt = count($list);
 ?>
 
 <style>
-	@media only screen and (max-width: 667px) {
-		.member_photo {
-			display: none;
-		}
-		.datetime {
-			color: black;
-			padding-top: 4px;
-		}
-		.datetime, .username {
-			font-size: 10px;
-		}
-		.eye {
-			font-size: 10px;
-			display: flex;
-			align-items: center;
-			width: 40px;
-			justify-content: flex-start;
-		}
-		.eye i {
-			padding-top: 3px;
-		}
-		.eye p {
-			padding-top: 4px; margin-left: 3px;
-		}
-	}
+@media only screen and (max-width: 667px) {
+    .member_photo {
+        display: none;
+    }
+
+    .datetime {
+        color: black;
+        padding-top: 4px;
+    }
+
+    .datetime,
+    .username {
+        font-size: 10px;
+    }
+
+    .eye {
+        font-size: 10px;
+        display: flex;
+        align-items: center;
+        width: 40px;
+        justify-content: flex-start;
+    }
+
+    .eye i {
+        padding-top: 3px;
+    }
+
+    .eye p {
+        padding-top: 4px;
+        margin-left: 3px;
+    }
+}
 </style>
 
-<section id="bo_list" class="mb-4" >
-	<!-- 목록 헤드 -->
-	<div class="d-block d-md-none w-100 mb-0 bg-<?php echo $head_color ?>" style="height:1px;"></div>
+<section id="bo_list" class="mb-4">
+    <!-- 목록 헤드 -->
+    <div class="d-block d-md-none w-100 mb-0 bg-<?php echo $head_color ?>" style="height:1px;"></div>
 
-	<div class="na-table d-none d-md-table w-100 mb-0">
-		<div class="<?php echo $head_class ?> d-md-table-row">
-			<div class="d-md-table-cell nw-5 px-md-1">번호</div>
-			<div class="d-md-table-cell pr-md-1">
-				<?php if ($is_checkbox) { ?>
-					<label class="float-left mb-0">
-						<input type="checkbox" onclick="if (this.checked) all_checked(true); else all_checked(false);">
-					</label>
-				<?php } ?>
-				제목
-			</div>
-			<div class="d-md-table-cell nw-10 pl-2 pr-md-1 text-left">이름</div>
-			<div class="d-md-table-cell nw-6 pr-md-1"><?php echo subject_sort_link('wr_datetime', $qstr2, 1) ?>날짜</a></div>
-			<div class="d-md-table-cell nw-4 pr-md-1"><?php echo subject_sort_link('wr_hit', $qstr2, 1) ?>조회</a></div>
-		</div>
-	</div>
+    <div class="na-table d-none d-md-table w-100 mb-0">
+        <div class="<?php echo $head_class ?> d-md-table-row">
+            <div class="d-md-table-cell nw-5 px-md-1">번호</div>
+            <div class="d-md-table-cell pr-md-1">
+                <?php if ($is_checkbox) { ?>
+                <label class="float-left mb-0">
+                    <input type="checkbox" onclick="if (this.checked) all_checked(true); else all_checked(false);">
+                </label>
+                <?php } ?>
+                제목
+            </div>
+            <div class="d-md-table-cell nw-10 pl-2 pr-md-1 text-left">이름</div>
+            <div class="d-md-table-cell nw-6 pr-md-1"><?php echo subject_sort_link('wr_datetime', $qstr2, 1) ?>날짜</a>
+            </div>
+            <div class="d-md-table-cell nw-4 pr-md-1"><?php echo subject_sort_link('wr_hit', $qstr2, 1) ?>조회</a></div>
+        </div>
+    </div>
 
-	<ul class="na-table d-md-table w-100">
-		<?php
+    <ul class="na-table d-md-table w-100">
+        <?php
 		for ($i=0; $i < $list_cnt; $i++) {
 			// hulan nemsen member table.회원 정보 가져오기
 			$mb=get_member($list[$i]['mb_id']);
@@ -131,34 +139,35 @@ $list_cnt = count($list);
 				$list[$i]['num'] = '<span class="sr-only">번호</span>'.$list[$i]['num'];
 			}
 		?>
-		<?php if (!strstr($list[$i]['wr_option'], "secret") || $is_admin)  {?>
-		<li class="d-md-table-row px-3 py-2 p-md-0 text-md-center text-muted border-bottom<?php echo $li_css;?>">
-			<div class="d-none d-md-table-cell nw-5 f-sm font-weight-normal py-md-2 px-md-1">
-				<?php echo $list[$i]['num'] ?>
-			</div>
-			 
-			<div class="d-md-table-cell text-left py-md-2 pr-md-1">
-				<div class="na-title float-md-left">
-					<div class="na-item">
-						<?php if ($is_checkbox) { ?>
-							<input type="checkbox" class="mb-0 mr-2" name="chk_wr_id[]" value="<?php echo $list[$i]['wr_id'] ?>" id="chk_wr_id_<?php echo $i ?>">
-						<?php } ?>
-						<a href="<?php echo $list[$i]['href'] ?>"<?php 
+        <?php if (!strstr($list[$i]['wr_option'], "secret") || $is_admin)  {?>
+        <li class="d-md-table-row px-3 py-2 p-md-0 text-md-center text-muted border-bottom<?php echo $li_css;?>">
+            <div class="d-none d-md-table-cell nw-5 f-sm font-weight-normal py-md-2 px-md-1">
+                <?php echo $list[$i]['num'] ?>
+            </div>
+
+            <div class="d-md-table-cell text-left py-md-2 pr-md-1">
+                <div class="na-title float-md-left">
+                    <div class="na-item">
+                        <?php if ($is_checkbox) { ?>
+                        <input type="checkbox" class="mb-0 mr-2" name="chk_wr_id[]"
+                            value="<?php echo $list[$i]['wr_id'] ?>" id="chk_wr_id_<?php echo $i ?>">
+                        <?php } ?>
+                        <a href="<?php echo $list[$i]['href'] ?>" <?php 
 						// secret bolson bgaad admin bol doorh style 
 						
 						if (strstr($list[$i]['wr_option'], "secret") && $is_admin) { echo "style='color:#bababa;text-decoration: line-through' "; }
 						//elseif(strstr($list[$i]['wr_option'], "secret") && $is_member) { echo "style='display: none;' "; } 
-						if ($list[$i]['wr_1']) { echo " style='color:".$list[$i]['wr_1']."' "; } ?> 
-						class="na-subject"<?php echo $target ?>>
-							<?php
+						if ($list[$i]['wr_1']) { echo " style='color:".$list[$i]['wr_1']."' "; } ?> class="na-subject"
+                            <?php echo $target ?>>
+                            <?php
 								if($list[$i]['icon_reply'])
 									echo '<span class="na-hicon na-reply"></span>'.PHP_EOL;
 
 								echo $wr_icon;
 								
 							?>
-							<!-- hulan nemsen 후기, 출근부 업소명 출력부분 -->
-							<!-- <?php 
+                            <!-- hulan nemsen 후기, 출근부 업소명 출력부분 -->
+                            <!-- <?php 
 								
 							 if($board['gr_id'] == "review" ){?>
 							<?php echo "[",$list[$i]['wr_7'],"-",$list[$i]['wr_5'], "] ", "&nbsp;&nbsp", $list[$i]['subject'] ;}
@@ -167,57 +176,58 @@ $list_cnt = count($list);
 								else{echo  $list[$i]['subject'] ;}
 							?> -->
 
-							<?php 
+                            <?php 
 								
 								if($board['gr_id'] == "review" ){ ?><span style="color: #000;">
-							   <?php echo "[".$list[$i]['wr_7']."-".$list[$i]['wr_5']. "]" ?></span><?php echo $list[$i]['subject'];}
+                                <?php echo "[".$list[$i]['wr_7']."-".$list[$i]['wr_5']. "]" ?></span><?php echo $list[$i]['subject'];}
 							   elseif($board['gr_id'] == "attendance"){ ?>
-							   <span style="color: #000;">
-								   <?php echo "[".$mb['mb_name']."]" ?> </span><?php echo $list[$i]['subject'] ;} 
+                            <span style="color: #000;">
+                                <?php echo "[".$mb['mb_name']."]" ?> </span><?php echo $list[$i]['subject'] ;} 
 								   else {echo  $list[$i]['subject'] ;}
 							?>
-							
-						</a>
-						<?php
+
+                        </a>
+                        <?php
 							if(isset($list[$i]['icon_file']))
 								echo '<span class="na-ticon na-file"></span>'.PHP_EOL;
 
 							//if(isset($list[$i]['icon_link']) && $list[$i]['icon_link'])
 								//echo '<span class="na-ticon na-link"></span>'.PHP_EOL;
 						?>
-						<?php if($list[$i]['wr_comment']) { ?>
-							<div class="na-info">
-								<span class="sr-only">댓글</span>
-								<span class="count-plus orangered">
-									<?php echo $list[$i]['wr_comment'] ?>
-								</span>
-							</div>
-						<?php } ?>
-					</div>
-				</div>
-			</div>
-			<div class="float-right float-md-none d-md-table-cell nw-16 nw-md-auto text-left f-sm">
-				<div style="width: 120px; text-align:left;">
-				<span class="sr-only">등록자</span>
-					<?php
+                        <?php if($list[$i]['wr_comment']) { ?>
+                        <div class="na-info">
+                            <span class="sr-only">댓글</span>
+                            <span class="count-plus orangered">
+                                <?php echo $list[$i]['wr_comment'] ?>
+                            </span>
+                        </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+            <div class="float-right float-md-none d-md-table-cell nw-16 nw-md-auto text-left f-sm">
+                <div style="width: 120px; text-align:left;">
+                    <span class="sr-only">등록자</span>
+                    <?php
 						$mbid= get_member($list[$i]['mb_id']);
 						$name = get_sideview($mbid['mb_id'], $mbid['mb_nick'], $mbid['mb_homepage']);
 						echo na_name_photo($list[$i]['mb_id'], "<p class='username'>".$name."</p>")
 					?>
-					</div>
-			</div>
-			<div class="float-left float-md-none d-md-table-cell nw-6 nw-md-auto f-sm">
-				<span class="sr-only">등록일</span>
-				<p class="datetime"><?php echo na_date($list[$i]['wr_datetime'], 'orangered', 'H:i', 'm.d', 'Y.m.d') ?></p>
-			</div>
-			<div class="float-left float-md-none d-md-table-cell nw-4 nw-md-auto f-sm">
-				<span class="sr-only">조회</span>
-				<div class="eye">
-					<i class="fa fa-eye d-md-none" aria-hidden="true"></i>	
-					<?php echo "<p>".$list[$i]['wr_hit']."</p>" ?>
-				</div>
-			</div>
-			<!-- <?php if($is_good) { ?>
+                </div>
+            </div>
+            <div class="float-left float-md-none d-md-table-cell nw-6 nw-md-auto f-sm">
+                <span class="sr-only">등록일</span>
+                <p class="datetime"><?php echo na_date($list[$i]['wr_datetime'], 'orangered', 'H:i', 'm.d', 'Y.m.d') ?>
+                </p>
+            </div>
+            <div class="float-left float-md-none d-md-table-cell nw-4 nw-md-auto f-sm">
+                <span class="sr-only">조회</span>
+                <div class="eye">
+                    <i class="fa fa-eye d-md-none" aria-hidden="true"></i>
+                    <?php echo "<p>".$list[$i]['wr_hit']."</p>" ?>
+                </div>
+            </div>
+            <!-- <?php if($is_good) { ?>
 				<div class="float-left float-md-none d-md-table-cell nw-3 nw-md-auto f-sm font-weight-normal py-md-2 pr-md-1">
 					<i class="fa fa-thumbs-o-up d-md-none" aria-hidden="true"></i>
 					<span class="sr-only">추천</span>
@@ -231,12 +241,12 @@ $list_cnt = count($list);
 					<small><?php echo $list[$i]['wr_nogood'] ?></small>
 				</div>
 			<?php } ?> -->
-			<div class="clearfix d-block d-md-none"></div>
-		</li>
-		<?php } ?>
-		<?php } ?>
-	</ul>
-	<?php if (!$list_cnt) { ?>
-		<div class="f-de font-weight-normal px-3 py-5 text-muted text-center border-bottom">게시물이 없습니다.</div>
-	<?php } ?>
+            <div class="clearfix d-block d-md-none"></div>
+        </li>
+        <?php } ?>
+        <?php } ?>
+    </ul>
+    <?php if (!$list_cnt) { ?>
+    <div class="f-de font-weight-normal px-3 py-5 text-muted text-center border-bottom">게시물이 없습니다.</div>
+    <?php } ?>
 </section>
