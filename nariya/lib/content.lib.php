1f3108231b188f943cde6436240f3f7ee53c20f5
Nurbyek <nuugii.k@gmail.com>
2021-03-19 12:12:16 +0800

    Update na_name_photo function
    Update na_name_photo function

--- nariya/lib/content.lib.php
+++ nariya/lib/content.lib.php
@@ -451,7 +451,7 @@ function na_name_photo($mb_id, $name){
 		preg_match("/alt=[\"\']?([^\"\']*)[\"\']?/", $matches[1][$i], $m);
 		
 		if($m[1]) {
-			return str_replace($matches[0][$i], '<img class="member_photo" src="'.na_member_photo($mb_id).'" width="'.$config['cf_member_icon_width'].'" height="'.$config['cf_member_icon_height'].'" class="d-md-none" style="border-radius:50%;" alt=""/> '.$levelimg,$name);
+			return str_replace($matches[0][$i], '<img class="member_photo" src="'.na_member_photo($mb_id).'" width="'.$config['cf_member_icon_width'].'" height="'.$config['cf_member_icon_height'].'" class="d-none" style="border-radius:50%;" alt=""/> '.$levelimg,$name);
 		}
     }
 	return $name;
