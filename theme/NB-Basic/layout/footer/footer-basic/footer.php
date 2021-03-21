dcee4fe007ac0951ecb8d55f93e05958c2e37888
Nurbyek <nuugii.k@gmail.com>
2021-03-21 12:46:35 +0800

    Update footer
    Update footer

--- theme/NB-Basic/layout/footer/footer-basic/footer.php
+++ theme/NB-Basic/layout/footer/footer-basic/footer.php
@@ -5,7 +5,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$nt_footer_url.'/footer.css">', 0
 ?>
 
 <?php if(G5_IS_MOBILE) { ?>
-	<div id="actionbtns" class="mb-3">
+	<div id="actionbtns">
 		<?php if($member['mb_id']) { ?>
 			<a href="<?php echo G5_BBS_URL.'/logout.php' ?>" class="button_col">로그아웃</a>
 			<a href="<?php echo G5_BBS_URL.'/userinfo.php' ?>" class="button_col">마이페이지</a>
