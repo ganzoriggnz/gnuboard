143dee76c8b77d8a0d8d9502850afea0b752383d
Nurbyek <nuugii.k@gmail.com>
2021-03-22 11:16:32 +0800

    Clear write button
    Clear write button

--- skin/board/gallery/list.skin.php
+++ skin/board/gallery/list.skin.php
@@ -155,9 +155,6 @@ if ($board['bo_use_category'] && $sca !='') {
                 <?php if ($rss_href) { ?><li><a href="<?php echo $rss_href ?>" class="btn_b01 btn" title="RSS"><i
                             class="fa fa-rss" aria-hidden="true"></i><span class="sound_only">RSS</span></a></li>
                 <?php } ?>
-                <?php if ($write_href) { ?><li><a href="<?php echo $write_href ?>" class="btn_b01 btn" title="글쓰기"><i
-                            class="fa fa-pencil" aria-hidden="true"></i><span class="sound_only">글쓰기</span></a></li>
-                <?php } ?>
                 <?php if ($is_admin == 'super' || $is_auth) {  ?>
                 <li>
                     <button type="button" class="btn_more_opt is_list_btn btn_b01 btn" title="게시판 리스트 옵션"><i
