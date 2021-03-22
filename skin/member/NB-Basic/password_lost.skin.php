1e52a6b94c60a622d53ade4d21d7019275ca888c
Ganzorig Baatar <ganzoriggnz@gmail.com>
2021-03-22 11:51:10 +0800

    phone skin change functoin bug fixed

--- skin/member/NB-Basic/password_lost.skin.php
+++ skin/member/NB-Basic/password_lost.skin.php
@@ -7,37 +7,39 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 ?>
 
 <div id="find_info" class="f-de">
-	<div id="topNav" class="bg-primary text-white">
-		<div class="p-3">
-			<button type="button" class="close" aria-label="Close" onclick="window.close();">
-				<span aria-hidden="true" class="text-white">&times;</span>
-			</button>
-			<h5><?php echo $g5['title'] ?></h5>
-		</div>
-	</div>
+    <div id="topNav" class="bg-primary text-white">
+        <div class="p-3">
+            <button type="button" class="close" aria-label="Close" onclick="window.close();">
+                <span aria-hidden="true" class="text-white">&times;</span>
+            </button>
+            <h5><?php echo $g5['title'] ?></h5>
+        </div>
+    </div>
 
-	<div id="topHeight"></div>
+    <div id="topHeight"></div>
 
-	<form name="fpasswordlost" action="<?php echo $action_url ?>" onsubmit="return fpasswordlost_submit(this);" method="post" autocomplete="off">
-		<div class="p-3">
+    <form name="fpasswordlost" action="<?php echo $action_url ?>" onsubmit="return fpasswordlost_submit(this);"
+        method="post" autocomplete="off">
+        <div class="p-3">
 
-			회원가입 시 등록하신 이메일 주소를 입력해 주세요. 해당 이메일로 아이디와 비밀번호 정보를 보내드립니다.
+            회원가입 시 등록하신 이메일 주소를 입력해 주세요. 해당 이메일로 아이디와 비밀번호 정보를 보내드립니다.
 
-			<label class="sr-only" for="mb_email">이메일<strong class="sr-only"> 필수</strong></label>
-			<div class="input-group my-3">
-				<div class="input-group-prepend">
-					<span class="input-group-text"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
-				</div>
-				<input type="text" name="mb_email" id="mb_email" required class="form-control required email" maxlength="100">
-				<div class="input-group-append">
-					<button type="submit" id="btn_sumbit" class="btn btn-primary">확인하기</button>
-				</div>
-			</div>
-			<div class="text-center">
-				<?php echo captcha_html(); ?>
-			</div>
-		</div>
-	</form>
+            <label class="sr-only" for="mb_email">이메일<strong class="sr-only"> 필수</strong></label>
+            <div class="input-group my-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+                </div>
+                <input type="text" name="mb_email" id="mb_email" required class="form-control required email"
+                    maxlength="100">
+                <div class="input-group-append">
+                    <button type="submit" id="btn_sumbit" class="btn btn-primary">확인하기</button>
+                </div>
+            </div>
+            <div class="text-center">
+                <?php echo captcha_html(); ?>
+            </div>
+        </div>
+    </form>
 </div>
 
 <script>
@@ -52,13 +54,13 @@ $(function() {
     var sh = screen.height;
     var cw = document.body.clientWidth;
     var ch = document.body.clientHeight;
-    var top  = sh / 2 - ch / 2 - 100;
+    var top = sh / 2 - ch / 2 - 100;
     var left = sw / 2 - cw / 2;
     moveTo(left, top);
 });
 
-$(window).on('load', function () {
-	na_nav('topNav', 'topHeight', 'fixed-top');
+$(window).on('load', function() {
+    na_nav('topNav', 'topHeight', 'fixed-top');
 });
 </script>
 <!-- } 회원정보 찾기 끝 -->
--- skin/member/NB-Basic/phone_change_request.skin.php
+++ skin/member/NB-Basic/phone_change_request.skin.php
@@ -1,41 +1,72 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+include_once('./_common.php');
+
+
+include_once('./_head.sub.php');
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
+add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
+
 
 @include_once(G5_THEME_PATH.'/common.php');
 
 ?>
 
-<div style="background-color:#f0f0f0; color:#000; font-weight:bold; padding:10px 0px 10px 0px; margin:0px; position:absolute; left:0px; top:0px; width:100%;" > <?php echo $g5['title']?></div>
+<div id="find_info" class="f-de">
+    <div id="topNav" class="bg-primary text-white">
+        <div class="p-3">
+            <button type="button" class="close" aria-label="Close" onclick="window.close();">
+                <span aria-hidden="true" class="text-white">&times;</span>
+            </button>
+            <h5><?php echo $g5['title']?></h5>
+        </div>
+    </div>
 
-<form name="fmember" id="fmember" method="post" action="<?php echo G5_BBS_URL ?>/member_hp_update.php">
-<input type="hidden" name="mb_id" value="<?php echo $member['mb_id']?>" id="mb_id">
-<div class="tbl_head02 tbl_wrap" style="margin-top:50px">
-    <table style="width:100%;margin:0 auto; font-size:13px;border:1px solid #707070" >
-	<tr><td style="background-color:#f0f0f0;padding:5px">신청자</td><td><?php echo $member['mb_nick']?>( <?php echo $member['mb_id']?> )</td></tr>
-	<tr><td style="background-color:#f0f0f0;padding:5px">업소명</td><td><?php echo $member['mb_name']?></td></tr>
-	<tr><td style="background-color:#f0f0f0;padding:5px">이전 전화번호</td><td><?php echo $member['mb_hp']?></td></tr>
-	<tr><td style="background-color:#f0f0f0;padding:5px">변경 전화번호</td>
-		<td><input type="text" name="new_hp" id="new_hp" value=""  style="width:120px;height:25px; text-align:left; margin-right:25px; border:1px solid #b5b5b5"  size="14" maxlength="14"></td></tr>
-	</table>
-<br/>
-<div style="text-align:center">
-	<input type="button" id="change" value="전화번호 변경요청" class="btn btn-primary btn-lg btn-block en">
-	<button type="button" class="btn btn-primary btn-lg btn-block en" onclick="window.close();">창닫기</button>
-</div>	
+    <form name="fmember" id="fmember" method="post" action="<?php echo G5_BBS_URL ?>/member_hp_update.php">
+        <input type="hidden" name="mb_id" value="<?php echo $member['mb_id']?>" id="mb_id">
+        <div class=" tbl_wrap" style="margin-top:10px">
+            <table style="width:100%;margin:0 auto; font-size:13px;border:1px solid #707070">
+                <tr>
+                    <td style="background-color:#f0f0f0;padding:5px">신청자</td>
+                    <td><?php echo $member['mb_nick']?>( <?php echo $member['mb_id']?> )</td>
+                </tr>
+                <tr>
+                    <td style="background-color:#f0f0f0;padding:5px">업소명</td>
+                    <td><?php echo $member['mb_name']?></td>
+                </tr>
+                <tr>
+                    <td style="background-color:#f0f0f0;padding:5px">이전 전화번호</td>
+                    <td><?php echo $member['mb_hp']?></td>
+                </tr>
+                <tr>
+                    <td style="background-color:#f0f0f0;padding:5px">변경 전화번호</td>
+                    <td><input type="tel" name="new_hp" id="new_hp" value="<?php echo $member['mb_10']?>"
+                            style="width:120px;height:25px; text-align:left; margin-right:25px; border:1px solid #b5b5b5"
+                            size="14" maxlength="14" pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"></td>
+                </tr>
+            </table>
+            <br />
+            <div style="text-align:center">
+                <input type="submit" id="change" value="전화번호 변경요청" class="btn btn-primary en">
+                <button type="button" class="btn btn-primary en" onclick="window.close();">창닫기</button>
+            </div>
 
-<!-- <?php if($_POST['new_hp']!='')
+            <!-- <?php if($_POST['new_hp']!='')
  echo '<br/><div style="text-align:center">전화번호 변경요청 완료되었습니다</div>'; 
-?> -->		
+
+ 
+?> -->
+        </div>
+    </form>
 </div>
-</form>
+
 <script src="<?php echo NA_URL ?>/js/jquery-3.5.1.min.js"></script>
 <script>
-		$('#change').click(function(){
-			if (confirm("이전 전화 번호를 변경하시겠습니까?")) {
-				$('#fmember').submit();
-			}                  
-		});                                      
+$('#change').click(function() {
+    if (confirm("이전 전화 번호를 변경하시겠습니까?")) {
+        $('#fmember').submit();
+    }
+});
 </script>
