750522b84cb70e9ba4d9806d5993938c599abb94
Nurbyek <nuugii.k@gmail.com>
2021-04-02 11:21:24 +0800

    twitter text change
    twitter text change

--- plugin/attendance/m_attendance.php
+++ plugin/attendance/m_attendance.php
@@ -350,7 +350,7 @@ function dateGo(day)
 </form>
 <script type="text/javascript">
 value=new Array();
-value[0]="밤의제국 트위터 twitter.com/bamje4 "
+value[0]="밤의제국 트위터 https://twitter.com/bamje4 즐겨찾기"
 value[1]="밤의제국 바로가기 bamje.com 검색"
 value[2]="밤의제국 접속주소 bamje1.com 다음접속 주소는 bamje2.com 입니다."
 value[3]="밤의제국 접속 안될 시 bamje.com 에서 뒷부분 .com 앞에 숫자를 하나씩 늘려가며 접속해보시면 됩니다."
