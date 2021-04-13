395f4561292c82cb9a9f9b819c742c257e2f7b9b
Nurbyek <nuugii.k@gmail.com>
2021-04-13 20:11:02 +0800

    Change sidebar board name
    Change sidebar board name

--- theme/NB-Basic/layout/sidebar/sidebar-basic/sidebar.php
+++ theme/NB-Basic/layout/sidebar/sidebar-basic/sidebar.php
@@ -338,7 +338,7 @@ $menu_cnt = count($menu);
 											<i class="fa fa-caret-right tree-toggle me-i1"></i>
 										<?php } ?>
 
-										<?php if($me1['text']=="실장업무게시판")
+										<?php if($me1['text']=="실장님 정보공유")
 												{if($member['mb_level']==26 || $member['mb_level']==27 || $is_admin) 
 													{ ?>
 										<a class="me-a1" href="<?php if($member['mb_level']==27 || $is_admin)  echo $me1['href'];?>" <?php if($member['mb_level']==26)  echo 'onclick=levelalert();'?> target="<?php echo $me1['target']; ?>">
