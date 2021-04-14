9589e57c26e27c1bdfdb9e65a2963f82c3e3231e
Nurbyek <nuugii.k@gmail.com>
2021-04-15 01:10:32 +0800

    Update sidebar
    Update sidebar

--- theme/NB-Basic/layout/sidebar/sidebar-basic/sidebar.php
+++ theme/NB-Basic/layout/sidebar/sidebar-basic/sidebar.php
@@ -189,7 +189,9 @@ $menu_cnt = count($menu);
 								</a>
 							</li>
 							<li class="col-6 px-1">
-								<a href="<?php echo G5_BBS_URL ?>/userinfo.php" class="btn btn-block btn-basic f-sm mb-2">마이페이지</a>
+								<a href="<?php echo G5_BBS_URL ?>/userinfo.php" target="_self" class="btn btn-block btn-basic f-sm mb-2">
+									마이페이지
+								</a>
 							</li>
 							<li class="col-6 px-1" >
 								<a style="color:red !important;" href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=member_leave.php" class="btn btn-block btn-basic f-sm mb-2">
