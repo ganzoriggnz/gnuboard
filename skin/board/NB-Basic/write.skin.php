98018af28861fcc5e0ca0df64d5e76f0b2a5b8e3
Nurbyek <nuugii.k@gmail.com>
2021-04-23 15:29:33 +0800

    Change twitter text
    Change twitter text

--- skin/board/NB-Basic/write.skin.php
+++ skin/board/NB-Basic/write.skin.php
@@ -101,7 +101,7 @@ if ($bo_table == "twitter") {
 	if ($member['mb_id'] && !$is_admin) {
 		$res = sql_fetch("select mb_id from " . $g5['write_prefix'] . $bo_table . " where mb_id = '" . $member['mb_id'] . "' and wr_is_comment = '0'");
 		if ($res['mb_id'] != '') {
-			alert("트위터인사를 한번만 할 수 있습니다!");
+			alert("트위터인증 게시판은 한번만 글 작성할 수 있습니다.");
 		}
 	}
 }
