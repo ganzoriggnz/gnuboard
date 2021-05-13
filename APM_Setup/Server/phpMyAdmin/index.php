767468d229086e1cce1f815c4b87a4d125ee41d8
Naran <iamnarran@gmail.com>
2021-05-13 11:56:35 +0800

    change favicon

--- APM_Setup/Server/phpMyAdmin/index.php
+++ APM_Setup/Server/phpMyAdmin/index.php
@@ -1,5 +1,6 @@
 <?php
 /* vim: set expandtab sw=4 ts=4 sts=4: */
+
 /**
  * forms frameset
  *
@@ -63,19 +64,21 @@ unset($cfgRelation);
 /**
  * pass variables to child pages
  */
-$drops = array('lang', 'server', 'convcharset', 'collation_connection',
-    'db', 'table');
+$drops = array(
+    'lang', 'server', 'convcharset', 'collation_connection',
+    'db', 'table'
+);
 
 foreach ($drops as $each_drop) {
-    if (! array_key_exists($each_drop, $_GET)) {
+    if (!array_key_exists($each_drop, $_GET)) {
         unset($_GET[$each_drop]);
     }
 }
 unset($drops, $each_drop);
 
-if (! strlen($GLOBALS['db'])) {
+if (!strlen($GLOBALS['db'])) {
     $main_target = $GLOBALS['cfg']['DefaultTabServer'];
-} elseif (! strlen($GLOBALS['table'])) {
+} elseif (!strlen($GLOBALS['table'])) {
     $_GET['db'] = $GLOBALS['db'];
     $main_target = $GLOBALS['cfg']['DefaultTabDatabase'];
 } else {
@@ -98,95 +101,95 @@ $lang_iso_code = $GLOBALS['available_languages'][$GLOBALS['lang']][2];
 // start output
 header('Content-Type: text/html; charset=' . $GLOBALS['charset']);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"
-    xml:lang="<?php echo $lang_iso_code; ?>"
-    lang="<?php echo $lang_iso_code; ?>"
-    dir="<?php echo $GLOBALS['text_dir']; ?>">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang_iso_code; ?>" lang="<?php echo $lang_iso_code; ?>" dir="<?php echo $GLOBALS['text_dir']; ?>">
+
 <head>
-<link rel="icon" href="./favicon.ico" type="image/x-icon" />
-<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
-<title>phpMyAdmin <?php echo PMA_VERSION; ?> -
-    <?php echo htmlspecialchars($HTTP_HOST); ?></title>
-<meta http-equiv="Content-Type"
-    content="text/html; charset=<?php echo $GLOBALS['charset']; ?>" />
-<meta name="robots" content="noindex,nofollow" />
-<script type="text/javascript">
-// <![CDATA[
-    // definitions used in common.js
-    var common_query = '<?php echo PMA_escapeJsString(PMA_generate_common_url('', '', '&'));?>';
-    var opendb_url = '<?php echo PMA_escapeJsString($GLOBALS['cfg']['DefaultTabDatabase']); ?>';
-    var safari_browser = <?php echo PMA_USR_BROWSER_AGENT == 'SAFARI' ? 'true' : 'false' ?>;
-    var querywindow_height = <?php echo PMA_escapeJsString($GLOBALS['cfg']['QueryWindowHeight']); ?>;
-    var querywindow_width = <?php echo PMA_escapeJsString($GLOBALS['cfg']['QueryWindowWidth']); ?>;
-    var collation_connection = '<?php echo PMA_escapeJsString($GLOBALS['collation_connection']); ?>';
-    var lang = '<?php echo PMA_escapeJsString($GLOBALS['lang']); ?>';
-    var server = '<?php echo PMA_escapeJsString($GLOBALS['server']); ?>';
-    var table = '<?php echo PMA_escapeJsString($GLOBALS['table']); ?>';
-    var db    = '<?php echo PMA_escapeJsString($GLOBALS['db']); ?>';
-    var token = '<?php echo PMA_escapeJsString($_SESSION[' PMA_token ']); ?>';
-    var text_dir = '<?php echo PMA_escapeJsString($GLOBALS['text_dir']); ?>';
-    var pma_absolute_uri = '<?php echo PMA_escapeJsString($GLOBALS['cfg']['PmaAbsoluteUri']); ?>';
-    var pma_text_default_tab = '<?php echo PMA_escapeJsString(PMA_getTitleForTarget($GLOBALS['cfg']['DefaultTabTable'])); ?>';
-    var pma_text_left_default_tab = '<?php echo PMA_escapeJsString(PMA_getTitleForTarget($GLOBALS['cfg']['LeftDefaultTabTable'])); ?>';
-
-    // for content and navigation frames
-
-    var frame_content = 0;
-    var frame_navigation = 0;
-    function getFrames() {
-<?php if ($GLOBALS['text_dir'] === 'ltr') { ?>
-        frame_content = window.frames[1];
-        frame_navigation = window.frames[0];
-<?php } else { ?>
-        frame_content = window.frames[0];
-        frame_navigation = window.frames[1];
-<?php } ?>
+    <?php
+    if (G5_IS_MOBILE) {
+        echo '<link rel="icon" href="./favicon.ico" type="image/x-icon" />';
+        echo '<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />';
+    } else {
+        echo '<link rel="icon" href="./favicon.png" type="image/x-icon" />';
+        echo '<link rel="shortcut icon" href="./favicon.png" type="image/x-icon" />';
     }
-    var onloadCnt = 0;
-    var onLoadHandler = window.onload;
-    window.onload = function() {
-        if (onloadCnt == 0) {
-            if (typeof(onLoadHandler) == "function") {
-                onLoadHandler();
-            }
-            if (typeof(getFrames) != 'undefined' && typeof(getFrames) == 'function') {
-                getFrames();
-            }
-            onloadCnt++;
+    ?>
+    <title>phpMyAdmin <?php echo PMA_VERSION; ?> -
+        <?php echo htmlspecialchars($HTTP_HOST); ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $GLOBALS['charset']; ?>" />
+    <meta name="robots" content="noindex,nofollow" />
+    <script type="text/javascript">
+        // <![CDATA[
+        // definitions used in common.js
+        var common_query = '<?php echo PMA_escapeJsString(PMA_generate_common_url('', '', '&')); ?>';
+        var opendb_url = '<?php echo PMA_escapeJsString($GLOBALS['cfg']['DefaultTabDatabase']); ?>';
+        var safari_browser = <?php echo PMA_USR_BROWSER_AGENT == 'SAFARI' ? 'true' : 'false' ?>;
+        var querywindow_height = <?php echo PMA_escapeJsString($GLOBALS['cfg']['QueryWindowHeight']); ?>;
+        var querywindow_width = <?php echo PMA_escapeJsString($GLOBALS['cfg']['QueryWindowWidth']); ?>;
+        var collation_connection = '<?php echo PMA_escapeJsString($GLOBALS['collation_connection']); ?>';
+        var lang = '<?php echo PMA_escapeJsString($GLOBALS['lang']); ?>';
+        var server = '<?php echo PMA_escapeJsString($GLOBALS['server']); ?>';
+        var table = '<?php echo PMA_escapeJsString($GLOBALS['table']); ?>';
+        var db = '<?php echo PMA_escapeJsString($GLOBALS['db']); ?>';
+        var token = '<?php echo PMA_escapeJsString($_SESSION[' PMA_token ']); ?>';
+        var text_dir = '<?php echo PMA_escapeJsString($GLOBALS['text_dir']); ?>';
+        var pma_absolute_uri = '<?php echo PMA_escapeJsString($GLOBALS['cfg']['PmaAbsoluteUri']); ?>';
+        var pma_text_default_tab = '<?php echo PMA_escapeJsString(PMA_getTitleForTarget($GLOBALS['cfg']['DefaultTabTable'])); ?>';
+        var pma_text_left_default_tab = '<?php echo PMA_escapeJsString(PMA_getTitleForTarget($GLOBALS['cfg']['LeftDefaultTabTable'])); ?>';
+
+        // for content and navigation frames
+
+        var frame_content = 0;
+        var frame_navigation = 0;
+
+        function getFrames() {
+            <?php if ($GLOBALS['text_dir'] === 'ltr') { ?>
+                frame_content = window.frames[1];
+                frame_navigation = window.frames[0];
+            <?php } else { ?>
+                frame_content = window.frames[0];
+                frame_navigation = window.frames[1];
+            <?php } ?>
         }
-    };
-// ]]>
-</script>
-<script src="./js/common.js" type="text/javascript"></script>
+        var onloadCnt = 0;
+        var onLoadHandler = window.onload;
+        window.onload = function() {
+            if (onloadCnt == 0) {
+                if (typeof(onLoadHandler) == "function") {
+                    onLoadHandler();
+                }
+                if (typeof(getFrames) != 'undefined' && typeof(getFrames) == 'function') {
+                    getFrames();
+                }
+                onloadCnt++;
+            }
+        };
+        // ]]>
+    </script>
+    <script src="./js/common.js" type="text/javascript"></script>
 </head>
 <frameset cols="<?php
-if ($GLOBALS['text_dir'] === 'rtl') {
-    echo '*,';
-}
-echo $GLOBALS['cfg']['NaviWidth'];
-if ($GLOBALS['text_dir'] === 'ltr') {
-    echo ',*';
-}
-?>" rows="*" id="mainFrameset">
+                if ($GLOBALS['text_dir'] === 'rtl') {
+                    echo '*,';
+                }
+                echo $GLOBALS['cfg']['NaviWidth'];
+                if ($GLOBALS['text_dir'] === 'ltr') {
+                    echo ',*';
+                }
+                ?>" rows="*" id="mainFrameset">
     <?php if ($GLOBALS['text_dir'] === 'ltr') { ?>
-    <frame frameborder="0" id="frame_navigation"
-        src="navigation.php<?php echo $url_query; ?>"
-        name="frame_navigation" />
+        <frame frameborder="0" id="frame_navigation" src="navigation.php<?php echo $url_query; ?>" name="frame_navigation" />
     <?php } ?>
-    <frame frameborder="0" id="frame_content"
-        src="<?php echo $main_target; ?>"
-        name="frame_content" />
+    <frame frameborder="0" id="frame_content" src="<?php echo $main_target; ?>" name="frame_content" />
     <?php if ($GLOBALS['text_dir'] === 'rtl') { ?>
-    <frame frameborder="0" id="frame_navigation"
-        src="navigation.php<?php echo $url_query; ?>"
-        name="frame_navigation" />
+        <frame frameborder="0" id="frame_navigation" src="navigation.php<?php echo $url_query; ?>" name="frame_navigation" />
     <?php } ?>
     <noframes>
+
         <body>
             <p><?php echo $GLOBALS['strNoFrames']; ?></p>
         </body>
     </noframes>
 </frameset>
+
 </html>
