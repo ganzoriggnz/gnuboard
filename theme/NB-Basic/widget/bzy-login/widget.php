d8b55fa399d6d8e7b4a3bd0374bba5f634a5ff48
Nurbyek <nuugii.k@gmail.com>
2021-06-02 21:56:22 +0800

    Add css to login
    Add css to login

--- theme/NB-Basic/widget/bzy-login/widget.php
+++ theme/NB-Basic/widget/bzy-login/widget.php
@@ -90,7 +90,7 @@ function insert_nickname($wer)
 				<h5 class="hide-photo mb-2">
 					<b style="letter-spacing:-1px;"><?php echo str_replace('sv_member', 'sv_member en', $member['sideview']); ?></b>
 				</h5><span><?php echo get_level($member['mb_id']);?></span>
-				<span style="font-size:14px; color:#e4c980;"><?php echo ($member['mb_grade']) ? $member['mb_grade'] : $member['mb_level'].'등급' ;  ?></span><br>
+				<span class="login_level"><?php echo ($member['mb_grade']) ? $member['mb_grade'] : $member['mb_level'].'등급' ;  ?></span><br>
 				
 				<!-- hulan nemsen 출근부 수정 -->
 				<?php $now = G5_TIME_YMDHIS;
