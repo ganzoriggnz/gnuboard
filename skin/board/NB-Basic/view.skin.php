dc6f796cf4796903ea4811a0b081d060ba8053d7
Nurbyek <nuugii.k@gmail.com>
2021-06-08 01:20:43 +0800

    Update buttons
    Update buttons

--- skin/board/NB-Basic/view.skin.php
+++ skin/board/NB-Basic/view.skin.php
@@ -163,7 +163,7 @@ if ($gr_id=='review') {
 	                        style="display: inline; margin-left: 5px; border:1px solid #e5e5e5;font-size: 10px;"
 	                        class="btn"><img src="<?php echo G5_IMG_URL.'/solid/sms.svg' ?>" style="height:12px;" title="">
 	                        문자보내기</a></li>
-	                <li id="bo_v_btn" class="flex-grow-1" style="display: inline;">
+	                <li id="bo_v_btn" class="flex-grow-1">
 	                    <!-- 게시물 상단 버튼 시작 { -->
 	                    <?php ob_start(); ?>
 	                    <div class="btn-group" role="group" style="float:right;">
@@ -326,7 +326,7 @@ if ($gr_id=='review') {
 						style="display: inline; border:1px solid #e5e5e5; font-size: 10px;" class="btn"><i class="fa fa-list-alt" aria-hidden="true"></i> 업소정보</a>
 					</li>
 					<?php } ?>
-	                <li id="bo_v_btn" class="flex-grow-1" style="display: inline;">
+	                <li id="bo_v_btn" class="flex-grow-1">
 	                    <!-- 게시물 상단 버튼 시작 { -->
 	                    <?php ob_start(); ?>
 	                    <div class="btn-group" role="group" style="float-right;">
