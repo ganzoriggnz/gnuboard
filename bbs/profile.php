ced71d5407a6242ed59b8c9b5f4befb21ab20b79
tuguldurq <mtuguldur1097@gmail.com>
2021-07-09 16:21:18 +0800

    bug bamje1

--- bbs/profile.php
+++ bbs/profile.php
@@ -5,7 +5,7 @@ if (!$member['mb_id'])
 alert('회원만 조회하실 수 있습니다.', G5_BBS_URL."/login.php?url=".urlencode("{$_SERVER['REQUEST_URI']}"));
 
 if($_REQUEST['mb_id'] == 'admin' && $member['mb_id'] != 'admin'){
-    alert_close('운영자의 자기소개는 이용할 수 없습니다.');
+    alert_close('작사레벨부터 전체게시물을 볼수 있습니다.');
 }
 
 if ($member['mb_level'] < 13) {
