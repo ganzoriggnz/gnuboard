3960e0fd4a451f95e05dc47af02f5a3d44750e31
tuguldurq <mtuguldur1097@gmail.com>
2021-07-19 01:16:09 +0800

    bamje2 redirecty

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-header("location: https://bamje2.com");
+header("Location: https://bamje2.com/");
+die;
 include_once('./_common.php');
-
 define('_INDEX_', true);
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
