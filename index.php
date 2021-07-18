2922314c6eb3be7e7e7d29db8a9a067f02fedbed
tuguldurq <mtuguldur1097@gmail.com>
2021-07-19 01:52:49 +0800

    Warning msg

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-die;
-
-include_once("./warning.php");
-die;
+header("Location: https://bamje1.com/error.php");
 include_once('./_common.php');
 define('_INDEX_', true);
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
