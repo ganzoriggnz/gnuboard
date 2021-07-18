2d39683caa77207014ece215e7a9029090793059
tuguldurq <mtuguldur1097@gmail.com>
2021-07-19 02:58:31 +0800

    aaa

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-header("Location: https://bamje1.com/error.html");
-die;
+header("Location: https://bamje1.com/notfound.html");
 include_once('./_common.php');
 define('_INDEX_', true);
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
