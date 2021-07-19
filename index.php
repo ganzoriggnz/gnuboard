f9a05180a3317d6642eeec661c0a7e063de3cd68
tuguldurq <mtuguldur1097@gmail.com>
2021-07-19 08:07:25 +0800

    error .php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-// include_once("./notfound.html");
-echo "error";
-die;
+include_once("./error.php");
+// echo "error";
+die;  
 include_once('./_common.php');
 define('_INDEX_', true);
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
