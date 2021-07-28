48203765d6713f68130e9890deed23508cee20bf
tuguldurq <mtuguldur1097@gmail.com>
2021-07-28 08:34:46 +0800

    list number text center

--- skin/board/NB-Basic/list/basic/list.skin.php
+++ skin/board/NB-Basic/list/basic/list.skin.php
@@ -229,7 +229,7 @@ if ($gr_id == 'attendance') {
                 
                 if (!strstr($list[$i]['wr_option'], "secret") || $is_admin || ($list[$i]['mb_id'] == $member['mb_id'] && strstr($list[$i]['wr_option'], "secret") && $board['bo_table'] == 'twitter')) { ?>
                     <li class="d-md-table-row px-3 py-2 p-md-0 text-md-center text-muted border-bottom<?php echo $li_css; ?>">
-                        <div class="text-left d-md-table-cell pr-md-1 py-md-2">
+                        <div class="text-center d-md-table-cell pr-md-1 py-md-2">
                             <div class="na-title float-md-left">
                                 <div class="na-item">
                                     <?php if($gr_id == 'attendance' && $list[$i]['has_coupon'])  echo '<img src="'.G5_URL.'/nariya/img/coupon.png" class="title_icon icon_img" alt="쿠폰후기">'; ?>
@@ -396,7 +396,7 @@ if ($gr_id == 'attendance') {
             
             if (!strstr($list[$i]['wr_option'], "secret") || $is_admin || ($list[$i]['mb_id'] == $member['mb_id'] && strstr($list[$i]['wr_option'], "secret") && $board['bo_table'] == 'twitter')) { ?>
                 <li class="d-md-table-row px-3 py-2 p-md-0 text-md-center text-muted border-bottom<?php echo $li_css; ?>">
-                    <div class="text-left d-md-table-cell pr-md-1 py-md-2">
+                    <div class="text-center d-md-table-cell pr-md-1 py-md-2">
                         <div class="na-title float-md-left">
                             <div class="na-item">
                                 <?php if($gr_id == 'attendance' && $list[$i]['has_coupon'])  echo '<img src="'.G5_URL.'/nariya/img/coupon.png" class="title_icon icon_img" alt="쿠폰후기">'; ?>
@@ -565,7 +565,7 @@ if ($gr_id == 'attendance') {
             
             if (!strstr($list[$i]['wr_option'], "secret") || $is_admin || ($list[$i]['mb_id'] == $member['mb_id'] && strstr($list[$i]['wr_option'], "secret") && $board['bo_table'] == 'twitter')) { ?>
                 <li class="d-md-table-row px-3 py-2 p-md-0 text-md-center text-muted border-bottom<?php echo $li_css; ?>">
-                    <div class="text-left d-md-table-cell pr-md-1 py-md-2">
+                    <div class="text-center d-md-table-cell pr-md-1 py-md-2">
                         <div class="na-title float-md-left">
                             <div class="na-item">
                             <div class="d-md-table-cell nw-5 font-weight-normal <?php if($list[$i]['is_coupon'] && !G5_IS_MOBILE) echo ''; else echo 'd-none'; ?>">
