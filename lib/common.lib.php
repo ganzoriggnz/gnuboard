bb45f86a783bf6d8fb39a1541360b1036f3922f0
tuguldurq <mtuguldur1097@gmail.com>
2021-08-11 11:53:52 +0800

    mantru

--- lib/common.lib.php
+++ lib/common.lib.php
@@ -974,8 +974,11 @@ function is_admin($mb_id)
 function get_category_option($bo_table = '', $ca_name = '')
 {
     global $g5, $board, $is_admin;
+    $sql = " select bo_subject, bo_category_list from {$g5['board_table']} where bo_table = '$bo_table'";
+    $row = sql_fetch($sql);
+    $categories = explode("|", $row['bo_category_list']);
 
-    $categories = explode("|", $board['bo_category_list']); // 구분자가 | 로 되어 있음
+    // $categories = explode("|", $board['bo_category_list']); // 구분자가 | 로 되어 있음
     $str = "";
     for ($i = 0; $i < count($categories); $i++) {
         $category = trim($categories[$i]);
