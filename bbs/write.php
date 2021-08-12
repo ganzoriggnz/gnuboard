5d07c83da3a54fd7b8ba8678c18a1af0f909fc26
tuguldurq <mtuguldur1097@gmail.com>
2021-08-12 08:08:02 +0800

    write dump

--- bbs/write.php
+++ bbs/write.php
@@ -353,6 +353,7 @@ if (($member['mb_level'] == 27 || ($member['mb_level'] == '26' && $finish_date >
 $is_category = false;
 $category_option = '';
 if ($board['bo_use_category'] && $member['mb_level'] !== 27) {
+    var_dump($category_option);die;
     $ca_name = $member['mb_7'];
     if (isset($write['ca_name']))
         $ca_name = $write['ca_name'];
