b94ff2ca1593afec61b6a30322436a47106b9c60
tuguldurq <mtuguldur1097@gmail.com>
2021-08-13 10:52:08 +0800

    default img position

--- skin/board/NB-Basic/write.skin.php
+++ skin/board/NB-Basic/write.skin.php
@@ -941,7 +941,7 @@ if ($is_member)
                         <p id="char_count_desc" class="f-sm text-muted">이 게시판은 최소 <strong><?php echo $write_min; ?></strong>글자
                             이상, 최대 <strong><?php echo $write_max; ?></strong>글자 이하까지 글을 쓰실 수 있습니다.</p>
                     <?php } ?>
-                    <input type="hidden" id="is_attendance" value="<?php echo $board['gr_id']; ?>">
+                    <input type="text" id="is_attendance" value="<?php echo $board['gr_id']; ?>">
                     <?php echo $editor_html; // 에디터 사용시는 에디터로, 아니면 textarea 로 노출
                     ?>
 
