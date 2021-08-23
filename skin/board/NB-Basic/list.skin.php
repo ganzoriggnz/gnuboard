ac15d03950c9a36b4145099007ff059f98c4bd72
tuguldurq <mtuguldur1097@gmail.com>
2021-08-23 10:57:45 +0800

    bug:is mobile

--- skin/board/NB-Basic/list.skin.php
+++ skin/board/NB-Basic/list.skin.php
@@ -173,40 +173,32 @@ add_javascript('<script src="' . G5_JS_URL . '/jquery.rumiTab.js"></script>', 0)
 			<!-- 게시판 페이지 정보 및 버튼 시작 { -->
 			<div id="bo_btn_top">
 				<div id="bo_list_total">
-					<?php if (!G5_IS_MOBILE) : ?>
-						<?php $row = sql_fetch("select * from {$g5['member_table']} where mb_id='{$board['bo_admin']}'"); ?>
-						<?php $row1 = sql_fetch("select * from {$g5['member_table']} where mb_id='{$group['gr_admin']}'"); ?>
+					<?php $row = sql_fetch("select * from {$g5['member_table']} where mb_id='{$board['bo_admin']}'"); ?>
+					<?php $row1 = sql_fetch("select * from {$g5['member_table']} where mb_id='{$group['gr_admin']}'"); ?>
+					<?php if ($board['bo_admin'] != '') : ?>
 						<?php
-							// $mbid1 = get_member($board['bo_admin']);
-							// $name1 = get_sideview($mbid1['mb_id'], $mbid1['mb_nick'], $mbid1['mb_homepage']);
-							$mbid2 = get_member($group['gr_admin']);
-							$name2 = get_sideview($mbid2['mb_id'], $mbid2['mb_nick'], $mbid2['mb_homepage']);
-						?>
-						<?php if ($board['bo_admin'] != '') : ?>
-							<?php
-								echo "이벤트관리자 관리자 : ";
-								$board_admins = explode(",",$board['bo_admin']);
-								foreach ($board_admins as $key => $value) :
-									$mbid1 = get_member($value);
-									$name1 = get_sideview($mbid1['mb_id'], $mbid1['mb_nick'], $mbid1['mb_homepage']);
-							 		echo na_name_photo($value, "<p class='username mr-3' style='display: inline-block;'>" . $name1 . "</p>");
-								endforeach; ?>
-						<?php endif; ?>
-						<?php if ($group['gr_admin'] != '') : ?>
-							<?php 
-								$group_admins = explode(",",$group['gr_admin']);
-								foreach ($group_admins as $key => $value) :
-									$mbid2 = get_member($value);
-									$name2 = get_sideview($mbid2['mb_id'], $mbid2['mb_nick'], $mbid2['mb_homepage']);
-							 		echo  "&nbsp;&nbsp;" . $group['gr_subject'] . " 관리자 : " . na_name_photo($value, "<p class='username mr-3' style='display: inline-block;'>" . $name2 . "</p>");
-								endforeach;
-							?>						
-						<?php endif; ?>
-						<?php echo "&nbsp;&nbsp;", "", "&nbsp;&nbsp;" ?>
+							echo "이벤트관리자 관리자 : ";
+							$board_admins = explode(",",$board['bo_admin']);
+							foreach ($board_admins as $key => $value) :
+								$mbid1 = get_member($value);
+								$name1 = get_sideview($mbid1['mb_id'], $mbid1['mb_nick'], $mbid1['mb_homepage']);
+								echo na_name_photo($value, "<p class='username mr-3' style='display: inline-block;'>" . $name1 . "</p>");
+							endforeach; ?>
+					<?php endif; ?>
+					<?php if ($group['gr_admin'] != '') : ?>
+						<?php 
+							$group_admins = explode(",",$group['gr_admin']);
+							foreach ($group_admins as $key => $value) :
+								$mbid2 = get_member($value);
+								$name2 = get_sideview($mbid2['mb_id'], $mbid2['mb_nick'], $mbid2['mb_homepage']);
+								echo  "&nbsp;&nbsp;" . $group['gr_subject'] . " 관리자 : " . na_name_photo($value, "<p class='username mr-3' style='display: inline-block;'>" . $name2 . "</p>");
+							endforeach;
+						?>						
+					<?php endif; ?>
+					<?php echo "&nbsp;&nbsp;", "", "&nbsp;&nbsp;" ?>
 
-						<?php if($bo_table != "free" && $bo_table != "event"):?>
-							<?php echo "[글 작성 " . $board['bo_write_point'] . " 파운드 /  댓글 작성 " . $board['bo_comment_point'] . " 파운드 획득]"; ?>
-						<?php endif; ?>
+					<?php if($bo_table != "free" && $bo_table != "event"):?>
+						<?php echo "[글 작성 " . $board['bo_write_point'] . " 파운드 /  댓글 작성 " . $board['bo_comment_point'] . " 파운드 획득]"; ?>
 					<?php endif; ?>
 				</div>
 				<div role="group">
