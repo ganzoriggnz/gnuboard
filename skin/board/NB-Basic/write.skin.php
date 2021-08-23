6408698cf61f8fcb54317b012f3eef810cc6968d
tuguldurq <mtuguldur1097@gmail.com>
2021-08-23 16:19:21 +0800

    feature: coupon check alert msg

--- skin/board/NB-Basic/write.skin.php
+++ skin/board/NB-Basic/write.skin.php
@@ -410,6 +410,7 @@ if ($is_member)
                             <div class="col-sm-10">
                                 <p class="float-left pt-1 pb-0 form-control-plaintext">
                                     <?php echo $option ?>
+                                    <span class="text-danger">쿠폰으로 업소 방문할 경우만 체크<span>
                                 </p>
                             </div>
                         </div>
@@ -905,7 +906,6 @@ if ($is_member)
                                 <p class="float-left pt-1 pb-0 form-control-plaintext">
                                     <?php echo $option ?>
                                     <span class="text-danger">쿠폰으로 업소 방문할 경우만 체크<span>
-
                                 </p>
                             </div>
                         </div>
