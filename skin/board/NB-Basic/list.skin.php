1cc98965b35ad37cf9e9bfb79c9c33cce0ee1534
tuguldurq <mtuguldur1097@gmail.com>
2021-08-23 12:59:30 +0800

    bug:mobile

--- skin/board/NB-Basic/list.skin.php
+++ skin/board/NB-Basic/list.skin.php
@@ -182,11 +182,12 @@ add_javascript('<script src="' . G5_JS_URL . '/jquery.rumiTab.js"></script>', 0)
 							foreach ($board_admins as $key => $value) :
 								$mbid1 = get_member($value);
 								$name1 = get_sideview($mbid1['mb_id'], $mbid1['mb_nick'], $mbid1['mb_homepage']);
-								echo na_name_photo($value, "<p class='username' style='display: inline-block;'>" . $name1 . "</p>") . $group['gr_admin'] != '' ? ", " : "";
+								echo na_name_photo($value, "<p class='username' style='display: inline-block;'>" . $name1 . "</p>");
 							endforeach; ?>
 					<?php endif; ?>
 					<?php if ($group['gr_admin'] != '') : ?>
 						<?php 
+							echo $board['bo_admin'] != '' ? ", " : "";
 							$group_admins = explode(",",$group['gr_admin']);
 							foreach ($group_admins as $key => $value) :
 								$mbid2 = get_member($value);
