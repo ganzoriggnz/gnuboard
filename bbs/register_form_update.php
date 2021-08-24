3ed966c74d2ad69994088a758d8a7d335cacbb0a
tuguldurq <mtuguldur1097@gmail.com>
2021-08-24 15:06:27 +0800

    feature: check lvl 26,27 in username

--- bbs/register_form_update.php
+++ bbs/register_form_update.php
@@ -295,7 +295,7 @@ if ($w == '') {
     set_session('ss_mb_reg', $mb_id);
 
 } else if ($w == 'u') {
-    if($member['mb_level'] < 27 || $member['mb_level'] < 28)
+    if($member['mb_level'] == 27 || $member['mb_level'] == 26)
         alert("관리자에게 문의 하시기 바랍니다.", "/bbs/userinfo.php");
 
     if (!trim($_SESSION['ss_mb_id']))
