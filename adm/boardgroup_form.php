a5ed3ce428c4ce4949cc87a7599c20a4fd1e3b38
tuguldurq <mtuguldur1097@gmail.com>
2021-08-26 15:27:15 +0800

    again: task122. admin config field length

--- adm/boardgroup_form.php
+++ adm/boardgroup_form.php
@@ -85,9 +85,9 @@ include_once('./admin.head.php');
         <td>
             <?php
             if ($is_admin == 'super')
-                echo '<input type="text" id="gr_admin" name="gr_admin" class="frm_input" value="'.$gr['gr_admin'].'" maxlength="20">';
+                echo '<input type="text" id="gr_admin" name="gr_admin" class="frm_input" value="'.$gr['gr_admin'].'" style="width:50%;">';
             else
-                echo '<input type="hidden" id="gr_admin" name="gr_admin" value="'.$gr['gr_admin'].'">'.$gr['gr_admin'];
+                echo '<input type="hidden" id="gr_admin" name="gr_admin" value="'.$gr['gr_admin'].'" style="width:50%;">'.$gr['gr_admin'];
             ?>
         </td>
     </tr>
