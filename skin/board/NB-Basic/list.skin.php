578a15485e2c76967b9563fc5519d47c941a2066
tuguldurq <mtuguldur1097@gmail.com>
2021-08-27 10:15:58 +0800

    bug: float right

--- skin/board/NB-Basic/list.skin.php
+++ skin/board/NB-Basic/list.skin.php
@@ -170,7 +170,7 @@ add_javascript('<script src="' . G5_JS_URL . '/jquery.rumiTab.js"></script>', 0)
 		<?php if ($bo_table != "pointrank" && $bo_table != "penyrank" && $bo_table != "levelrank" && $bo_table != "boardadmlist" && $bo_table != "mypage") { ?>
 
 			<!-- 게시판 페이지 정보 및 버튼 시작 { -->
-			<div id="bo_btn_top" <?php echo G5_IS_MOBILE ? "style='flex-wrap: wrap; text-align:right;'" : ""; ?>>
+			<div id="bo_btn_top" <?php echo G5_IS_MOBILE ? "style='flex-wrap: wrap;'" : ""; ?>>
 				<div id="bo_list_total" >
 					<?php if ($board['bo_admin'] != '' || $group['gr_admin'] != '') : ?> 
 						<?php echo "<span>" . "이벤트관리자 : " . "</span>"; ?>
@@ -270,7 +270,7 @@ add_javascript('<script src="' . G5_JS_URL . '/jquery.rumiTab.js"></script>', 0)
 						) { ?>
 							<?php if ($write_href) { ?>
 								<div>
-									<button type="button" class="btn btn-primary adminbtn" onclick="location.href='<?php echo $write_href ?>'">
+									<button type="button" class="float-right btn btn-primary adminbtn" onclick="location.href='<?php echo $write_href ?>'">
 										<img src="<?php echo G5_URL ?>/img/solid/pencil-alt.svg" style="height: 10px;">&nbsp;글쓰기
 									</button>
 								</div>
