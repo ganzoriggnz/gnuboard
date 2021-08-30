07efcd22dfb4cb7b411ad0b122fd430c9ca971d7
tuguldurq <mtuguldur1097@gmail.com>
2021-08-30 09:15:18 +0800

    bug: register form redesign

--- skin/member/NB-Basic/register_form.skin3.php
+++ skin/member/NB-Basic/register_form.skin3.php
@@ -133,7 +133,7 @@ add_stylesheet('<link rel="stylesheet" href="' . $member_skin_url . '/style.css"
 					</div>
 					<div class="col-sm-6 <?php echo G5_IS_MOBILE ? 'd-flex mt-2' : 'row'; ?>" >
 						<a onclick="window.open('<?php echo G5_URL ?>/bbs/member_hp_change.php?mb_id=<?php echo $member['mb_id'] ?>','전화번호 변경요청','width=500,height=300,scrollbars=no,padding=0, margin=0, top=300,left=800');" 
-						style="color:#000;background-color:#efefef; padding:5px; border:1px solid #696969; border-radius:5px; text-decoration:none; <?php echo G5_IS_MOBILE ? 'height: fit-content; width !important: 38%;' : ''; ?>" >
+						style="color:#000;background-color:#efefef; padding:5px; border:1px solid #696969; border-radius:5px; text-decoration:none; <?php echo G5_IS_MOBILE ? 'height: fit-content; width: 40% !important;' : ''; ?>" >
 							<font style="vertical-align: inherit;">전화번호 변경요청</font></a>						
 						<p class="text-muted" style="padding: .375rem 0;">
 						&nbsp;※ 전화번호는 운영자가 확인 후 변경처리됩니다.
