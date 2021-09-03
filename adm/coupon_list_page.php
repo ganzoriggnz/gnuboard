4cdb082050a7bdd49e6cc1d467bd1e07249d70ba
tuguldurq <mtuguldur1097@gmail.com>
2021-09-03 09:01:53 +0800

    bug: coupon list popup

--- adm/coupon_list_page.php
+++ adm/coupon_list_page.php
@@ -132,6 +132,7 @@ if( isset($_POST['id'])){
             <tbody>
         <?php     
         $result = "SELECT a.*, c.mb_level FROM {$g5['coupon_table']} a INNER JOIN $at_table b ON a.mb_id = b.mb_id INNER JOIN {$g5['member_table']} c ON a.mb_id = c.mb_id WHERE a.co_begin_datetime='{$co_begin_datetime}' AND a.co_end_datetime='{$co_end_datetime}' AND (a.co_sale_num > '0' or a.co_free_num > '0') AND b.wr_is_comment = '0' AND (c.mb_level = '26' OR c.mb_level = '27')"; 
+        echo $result;
         $result1=sql_query($result);
 
         while ($row = sql_fetch_array($result1)) {     
@@ -202,14 +203,28 @@ if( isset($_POST['id'])){
                 </td>
                 
                 <td class="td_left" style="text-align: left; width: 8rem;">
-                    <a data-type = "S" data-entity="<?php echo $row['co_entity'];?>" data-no = "<?php echo $row['co_no'];?>" data-mb-id = "<?php echo $row['mb_id'];?>" data-link="<?php echo $bo_table;?>" <?php if(number_format($row['co_sale_num']-$row['co_sent_snum']) == '0' || $co_send_date > $now) { echo 'style="font-weight: bold;"'; } else { echo 'data-toggle="modal" href="#couponModal" class="coupon-modal" style="color:blue; font-weight: bold;"';}  ?>>
-                        <?php echo "원가권 ".number_format($row['co_sale_num']-$row['co_sent_snum'])."개";?>
+                    <a 
+                        data-type = "S" 
+                        data-entity="<?php echo $row['co_entity'];?>" 
+                        data-no = "<?php echo $row['co_no'];?>" 
+                        data-mb-id = "<?php echo $row['mb_id'];?>" 
+                        data-link="<?php echo $bo_table;?>" 
+                        <?php if(number_format($row['co_sale_num']) == '0' || $co_send_date > $now) { echo 'style="font-weight: bold;"'; } else { echo 'data-toggle="modal" href="#couponModal" class="coupon-modal" style="color:blue; font-weight: bold;"';}  ?>>
+                        <?php echo "원가권 ".number_format($row['co_sale_num'])."개";?>
+                        <!-- -$row['co_sent_snum'] -->
                     </a>
                 </td> 
                 
                 <td class="td_left" style="text-align: left; width: 8rem;">
-                    <a data-type = "F" data-entity="<?php echo $row['co_entity'];?>" data-no = "<?php echo $row['co_no'];?>" data-mb-id = "<?php echo $row['mb_id'];?>" data-link="<?php echo $bo_table;?>" <?php if(number_format($row['co_free_num']-$row['co_sent_fnum']) == '0' || $co_send_date > $now){ echo 'style="font-weight: bold;"'; } else { echo 'data-toggle="modal" href="#couponModal" class="coupon-modal" style="color:blue; font-weight: bold;"';} ?>>
-                        <?php echo "무료권 ".number_format($row['co_free_num']-$row['co_sent_fnum'])."개";?>
+                    <a 
+                        data-type = "F" 
+                        data-entity="<?php echo $row['co_entity'];?>" 
+                        data-no = "<?php echo $row['co_no'];?>" 
+                        data-mb-id = "<?php echo $row['mb_id'];?>" 
+                        data-link="<?php echo $bo_table;?>" 
+                        <?php if(number_format($row['co_free_num']) == '0' || $co_send_date > $now){ echo 'style="font-weight: bold;"'; } else { echo 'data-toggle="modal" href="#couponModal" class="coupon-modal" style="color:blue; font-weight: bold;"';} ?>>
+                        <?php echo "무료권 ".number_format($row['co_free_num'])."개";?>
+                        <!-- -$row['co_sent_fnum'] -->
                     </a>
                 </td>
                 
