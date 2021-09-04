6231f97f40d0125f1fbe17eba9cac08464e3c76c
tuguldurq <mtuguldur1097@gmail.com>
2021-09-05 01:34:46 +0800

    bug: review button

--- theme/NB-Basic/layout/title/title-basic/title.php
+++ theme/NB-Basic/layout/title/title-basic/title.php
@@ -63,7 +63,7 @@ if ($write['wr_reply'] == '' && ($is_admin == 'super' || $is_admin == 'group' ||
 								<div class="clearfix f-sm text-muted pt-2 pr-2">
 									<h3 class="sr-only">컨텐츠 정보</h3>
 									<ul class="d-flex-start align-items-center mr-2"> -->
-										<li id="bo_v_btn" style="width:70%;display: flex;flex-wrap: wrap;justify-content:flex-end;">
+										<li id="bo_v_btn" style="width:70%;display: flex !important;flex-wrap: wrap !important;justify-content:flex-end !important;">
 											<!-- 게시물 상단 버튼 시작 { -->
 											<?php ob_start(); ?>
 											<div>
